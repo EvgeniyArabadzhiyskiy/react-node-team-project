11220caf44ad2f7ea45693cbc97ce8628ed98bfc
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-04 19:33:31 +0200

    delete files

--- src/components/Diagram/Diagram.jsx
+++
@@ -1,9 +0,0 @@
-const Diagram = () => {
-  return (
-    <div style={{ width: '500px', height: '200px', background: 'orange' }}>
-      <h1>Statistics</h1>
-    </div>
-  );
-};
-
-export default Diagram;
