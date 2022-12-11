c0885ed4f6c359f44552ea4885054d11826cc024
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-11 14:23:54 +0200

    Update wallet-api.js

--- src/redux/WalletApiServise/wallet-api.js
+++ src/redux/WalletApiServise/wallet-api.js
@@ -1,9 +1,10 @@
-// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-// import axios from 'axios'
-
+import {
+  createApi,
+  //  fetchBaseQuery
+} from '@reduxjs/toolkit/query/react';
+import axios from 'axios';
 
 const BASE_URL = 'https://wallet-project.onrender.com/api';
-console.log("BASE_URL", BASE_URL);
 
 // const token = {
 //   set(token) {
@@ -14,19 +15,94 @@ console.log("BASE_URL", BASE_URL);
 //   },
 // };
 
+const axiosBaseQuery =
+  ({ baseUrl }) =>
+  async ({ url, method, data, params }) => {
 
-// const axiosBaseQuery = ({ baseUrl }) => async ({ url, method, data, params }) => {
-  
-//   try {
-//     const result = await axios({ url: baseUrl + url, method, data, params })
-//     return { data: result.data }
-//   } catch (axiosError) {
-//     let err = axiosError
-//     return {
-//       error: {
-//         status: err.response?.status,
-//         data: err.response?.data || err.message,
-//       },
-//     }
-//   }
-// }
+    try {
+      const result = await axios({ url: baseUrl + url, method, data, params });
+      return { data: result.data };
+    } catch (axiosError) {
+      let err = axiosError;
+      return {
+        error: {
+          status: err.response?.status,
+          data: err.response?.data || err.message,
+        },
+      };
+    }
+  };
+
+export const walletsApi = createApi({
+  reducerPath: 'walletsApi',
+  baseQuery: axiosBaseQuery({ baseUrl: BASE_URL }),
+
+  tagTypes: ['Transaction'],
+
+  endpoints: builder => ({
+    // userRegistration: builder.mutation({
+    //   query: user => {
+    //     return { url: '/users/register', method: 'POST', data: user };
+    //     // return { url: '/users/register', method: 'POST', body: user };
+    //   },
+
+    //   transformResponse: (response, meta, arg) => {
+    //     token.set(response.token);
+    //     return response;
+    //   },
+    // }),
+
+    // userLogin: builder.mutation({
+    //   query: user => {
+    //     // console.log('user', user);
+    //     return { url: '/users/login', method: 'POST', data: user };
+    //   },
+
+    //   transformResponse: (response, meta, arg) => {
+    //     token.set(response.token);
+    //     return response;
+    //   },
+    // }),
+
+    // userLogout: builder.mutation({
+    //   query: () => {
+    //     // console.log('user', user);
+    //     return { url: '/users/logout', method: 'POST' };
+    //   },
+
+    //   transformResponse: (response, meta, arg) => {
+    //     token.unset();
+    //     return response;
+    //   },
+    // }),
+
+    getAllTransactions: builder.query({
+      query: (page = 1) => {
+        return { url: `/transactions?page=${page}&limit=10`, method: 'GET' };
+      },
+
+      providesTags: ['Transaction'],
+
+      transformResponse: (response, meta, arg) => {
+        // console.log( 'Response', response);
+        return response
+      },
+    }),
+
+    addTransact: builder.mutation({
+      query: body => ({ url: '/transactions', method: 'POST', data: body }),
+      invalidatesTags: ['Transaction'],
+    }),
+  }),
+});
+
+export const {
+//   useUserRegistrationMutation,
+//   useUserLoginMutation,
+//   useUserLogoutMutation,
+
+  useGetAllTransactionsQuery,
+  useAddTransactMutation,
+} = walletsApi;
+
+// export const { reducer, reducerPath } = walletsApi;
