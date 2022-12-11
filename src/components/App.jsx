647f3758cd9a284f66d4398ced43b442ecf649e0
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-11 14:23:30 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,8 +6,8 @@ import { ThemeProvider } from 'styled-components';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { refreshUser } from 'redux/auth/authOperation';
-import { getNextPage } from 'redux/transactions/transactionsSlice';
-import { getAllTransactions } from 'redux/transactions/transactionOperations';
+// import { getNextPage, testOperation } from 'redux/transactions/transactionsSlice';
+// import { getAllTransactions } from 'redux/transactions/transactionOperations';
 
 import HomeTab from './HomeTab';
 import Currency from './Currency';
@@ -19,58 +19,115 @@ import ButtonAddTransactions from './ButtonAddTransactions';
 import FormTransaction from './FormTransaction/FormTransaction';
 import { nightTheme, dayTheme } from '../theme';
 import Spinner from './Spinner';
+import { useGetAllTransactionsQuery } from 'redux/WalletApiServise/wallet-api';
+import { useState } from 'react';
 
 const LoginPage = lazy(() => import('../pages/LoginPage'));
 const DashboardPage = lazy(() => import('../pages/DashboardPage'));
 const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
+
+
+
+// const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOGEyYTA1OGQ4MmIyMjMxNWY2OWNlZSIsImlhdCI6MTY3MDE3ODg0NiwiZXhwIjoxNjcxMzg4NDQ2fQ.YeyuFPN6T0sjdm7X5onQhuYGfZhT73OoNHyBxkJGvtE'
+// axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
 export const App = () => {
   const isMobie = useMedia('(max-width: 767px)');
 
   const dispatch = useDispatch();
   const isDarkTheme = useSelector(store => store.theme.isNightTheme);
-  const { isLoggedIn, isError, isRefreshingUser } = useSelector(state => state.auth);
-  const { transactions, isModalAddOpen, hasNextPage, pageNum } = useSelector(state => state.transactions);
+  const { isError, isRefreshingUser } = useSelector(state => state.auth);
+  const { isModalAddOpen } = useSelector(state => state.transactions);
+  
+ 
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  const [page, setPage] = useState(1)
+  const [allTrans, setAllTrans] = useState([])
+  const [hasPage, setHasPage] = useState(false)
+
+  
 
   const observer = useRef(null);
 
   const lastElement = useCallback(item => {
 
     observer.current = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting && hasNextPage) {
+      if (entries[0].isIntersecting && hasPage ) {
         observer.current.unobserve(entries[0].target);
-        dispatch(getNextPage());
+        // dispatch(getNextPage());
+        setPage(prev => prev + 1)
+
       }
       }, { rootMargin: '5px',threshold: 1});
 
       if (item)  observer.current.observe(item);
       
     },
-    [dispatch, hasNextPage]
+    [hasPage]
   );
 
-  useEffect(() => {
-    if (isLoggedIn) dispatch(getAllTransactions(pageNum));
-  }, [dispatch, isLoggedIn, pageNum]);
+  // useEffect(() => {
+  //   if (isLoggedIn) dispatch(getAllTransactions(pageNum));
+  // }, [dispatch, isLoggedIn, pageNum]);
 
-  useEffect(() => {
-    dispatch(refreshUser());
-  }, [dispatch]);
-
-  if (transactions.length < 0) return null;
   
+
+  
+  const { data  } = useGetAllTransactionsQuery(page, 
+  //   {
+  //   selectFromResult: (response) => {
+  //     return  {
+  //       transactions: response.data?.transactions
+  //       // transactions: {
+  //       //   allTransactions: response.data?.transactions,
+  //       //   balance: response.data?.userBalance,
+  //       //   user: 'Djon',
+  //       //   age: 30,
+  //       // }
+  //     }
+  //   }
+  // }
+  )
+
+  // const transactions = useMemo(() => {
+  //   return  data?.transactions 
+  // },[data?.transactions])
+  const transactions = data?.transactions 
+
+
+  // const balance = useMemo(() => {
+  //   return  data?.userBalance || 0
+  // },[data?.userBalance])
+
+  const balance = data?.userBalance 
+  
+  useEffect(() => {
+    setHasPage(Boolean(transactions?.length))
+
+    if (transactions?.length > 0) {
+      setAllTrans(prev => [...prev, ...transactions])
+    }
+  }, [transactions]);
+    
+
+  // if (transactions.length < 0) return null;
+  if (!transactions) return null;
+
 
   if (isError)  toast.error(isError)
   
-
   return isRefreshingUser ? (
     <Spinner />
   ) : (
     <ThemeProvider theme={isDarkTheme ? dayTheme : nightTheme}>
       <Suspense fallback={null}>
-        
+
+      
+
         <Routes>
-          {/* <Route path="/" element={<Navigate to="/login" />} /> */}
 
           <Route
             path="/login"
@@ -87,13 +144,18 @@ export const App = () => {
               </PublicRoute>}
           />
 
-          <Route path="/" element={<DashboardPage />}>
+          <Route path="/" element={<DashboardPage totalBalance={balance} />}>
             <Route index element={<Navigate to="/login" />} />
             <Route
               path="home"
               element={
                 <PrivateRoute>
-                  <HomeTab data={transactions} ref={lastElement} />
+                 
+                  <HomeTab 
+                  data={allTrans} 
+                  totalBalance={balance}
+                  // data={transactions} 
+                  ref={lastElement} />
                   <ButtonAddTransactions />
                 </PrivateRoute>}
             />
@@ -125,10 +187,36 @@ export const App = () => {
 
       {isModalAddOpen && (
         <ModalAddTransaction>
-          <FormTransaction />
+          <FormTransaction setAllTrans={setAllTrans} setPage={setPage} />
         </ModalAddTransaction>
       )}
       <ToastContainer autoClose={2500} theme="colored" />
     </ThemeProvider>
   );
 };
+
+
+
+// const [addNewTransaction_RTK] =  useAddTransactMutation()
+
+// const handleAddPost = async () => {
+//   const trans = {
+//     comment: 'Mutation',
+//     amount: 200,
+//     category: 'Children',
+//     typeOperation: "expense",
+//     date: new Date().toString(),
+//   }
+
+//   await addNewTransaction_RTK(trans).un
+  
+// };
+
+
+// const handleAddPost =  () => {
+//   setPage(prev => prev + 1)
+// };
+
+// <button type='button' onClick={handleAddPost}>Add Post</button> 
+
+// <ul>{data.map(item => { return <li key={item._id}>{item.category}</li>})}</ul> 
