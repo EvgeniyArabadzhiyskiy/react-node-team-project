9e156310f3893a1c2ab2b0968df16f07b04ee425
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-11 14:23:51 +0200

    Update transactionsSlice.js

--- src/redux/transactions/transactionsSlice.js
+++ src/redux/transactions/transactionsSlice.js
@@ -1,10 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { addNewTransaction, getAllTransactions } from './transactionOperations';
+// import { addNewTransaction, getAllTransactions } from './transactionOperations';
 
 const initialState = {
   isModalAddOpen: false,
   totalBalance: 0,
-  transactions: [],
+  transactions: [1,2,9],
   pageNum: 1,
   hasNextPage: false,
   isLoading: false,
@@ -28,51 +28,51 @@ const transactionsSlice = createSlice({
     },
   },
 
-  extraReducers: builder => {
-    builder.addCase(addNewTransaction.pending, state => {
-      state.error = null;
-      state.isLoading = true;
-    });
+  // extraReducers: builder => {
+  //   builder.addCase(addNewTransaction.pending, state => {
+  //     state.error = null;
+  //     state.isLoading = true;
+  //   });
 
-    builder.addCase(addNewTransaction.fulfilled, (state, action) => {
-      state.isLoading = false;
-    });
-    
-    builder.addCase(addNewTransaction.rejected, (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    });
+  //   builder.addCase(addNewTransaction.fulfilled, (state, action) => {
+  //     state.isLoading = false;
+  //   });
 
-    builder.addCase(getAllTransactions.pending, (state, _) => {
-      state.error = null;
-      state.isLoading = true;
-    });
+  //   builder.addCase(addNewTransaction.rejected, (state, action) => {
+  //     state.isLoading = false;
+  //     state.error = action.payload;
+  //   });
 
-    builder.addCase(getAllTransactions.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.hasNextPage = Boolean(action.payload.transactions.length)
+  //   builder.addCase(getAllTransactions.pending, (state, _) => {
+  //     state.error = null;
+  //     state.isLoading = true;
+  //   });
 
-      if (state.pageNum === 1) {
-        state.transactions = [ ...action.payload.transactions];
-      } else {
-        state.transactions = [
-          ...state.transactions,
-          ...action.payload.transactions
-        ];
-      }
+  //   builder.addCase(getAllTransactions.fulfilled, (state, action) => {
+  //     state.isLoading = false;
+  //     state.hasNextPage = Boolean(action.payload.transactions.length);
 
-      if (action.payload.transactions.length > 0 && state.pageNum === 1) {
-        state.totalBalance = action.payload.userBalance;
-      }
-    });
-    
-    builder.addCase(getAllTransactions.rejected, (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    });
-  },
+  //     if (state.pageNum === 1) {
+  //       state.transactions = [...action.payload.transactions];
+  //     } else {
+  //       state.transactions = [
+  //         ...state.transactions,
+  //         ...action.payload.transactions,
+  //       ];
+  //     }
+
+  //     if (action.payload.transactions.length > 0 && state.pageNum === 1) {
+  //       state.totalBalance = action.payload.userBalance;
+  //     }
+  //   });
+
+  //   builder.addCase(getAllTransactions.rejected, (state, action) => {
+  //     state.isLoading = false;
+  //     state.error = action.payload;
+  //   });
+  // },
 });
 
-export const { toggleModalAdd, getNextPage, resetTransactions } = transactionsSlice.actions;
+export const { toggleModalAdd, getNextPage, resetTransactions, testOperation } = transactionsSlice.actions;
 
 export default transactionsSlice.reducer;
