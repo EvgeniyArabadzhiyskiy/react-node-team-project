28d2af78dd220ae202c0837cc182d0945af54afe
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-13 19:20:00 +0200

    Update transactionOperations.js

--- src/redux/transactions/transactionOperations.js
+++ src/redux/transactions/transactionOperations.js
@@ -1,26 +1,26 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+// import axios from 'axios';
 
-export const addNewTransaction = createAsyncThunk(
-  'transaction/ADD',
-  async (transaction, { rejectWithValue }) => {
-    try {
-      const { data } = await axios.post(`/transactions`, transaction);
-      return data;
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
-  }
-);
+// export const addNewTransaction = createAsyncThunk(
+//   'transaction/ADD',
+//   async (transaction, { rejectWithValue }) => {
+//     try {
+//       const { data } = await axios.post(`/transactions`, transaction);
+//       return data;
+//     } catch (error) {
+//       return rejectWithValue(error.message);
+//     }
+//   }
+// );
 
-export const getAllTransactions = createAsyncThunk(
-  'transaction/GET_ALL',
-  async (page = 1, { rejectWithValue }) => {
-    try {
-      const result = await axios.get(`/transactions?page=${page}&limit=10`);
-      return result.data;
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
-  }
-);
+// export const getAllTransactions = createAsyncThunk(
+//   'transaction/GET_ALL',
+//   async (page = 1, { rejectWithValue }) => {
+//     try {
+//       const result = await axios.get(`/transactions?page=${page}&limit=10`);
+//       return result.data;
+//     } catch (error) {
+//       return rejectWithValue(error.message);
+//     }
+//   }
+// );
