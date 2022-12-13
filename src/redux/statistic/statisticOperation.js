2074e7557d3c9512c34fe8e790e50b74027c9685
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-13 19:19:52 +0200

    Update statisticOperation.js

--- src/redux/statistic/statisticOperation.js
+++ src/redux/statistic/statisticOperation.js
@@ -1,25 +1,25 @@
-import axios from 'axios';
-import { createAsyncThunk } from '@reduxjs/toolkit';
+// import axios from 'axios';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const querySyring = ({month, year}) => {
-  if (month && year)  return `/transactions/statistics?month=${month}&year=${year}`
-  if (year)  return `/transactions/statistics?year=${year}`
-  if (month)  return `/transactions/statistics?month=${month}`
-  return '/transactions/statistics'
-}
-export const getStatistic = createAsyncThunk(
-  'transactions/statistics',
-  async (data={}, { rejectWithValue }) => {
-    const query = querySyring(data);
-    try {
-      const res = await axios.get(query);
-      return res.data;
+// const queryString = ({month, year}) => {
+//   if (month && year)  return `/transactions/statistics?month=${month}&year=${year}`
+//   if (year)  return `/transactions/statistics?year=${year}`
+//   if (month)  return `/transactions/statistics?month=${month}`
+//   return '/transactions/statistics'
+// }
+// export const getStatistic = createAsyncThunk(
+//   'transactions/statistics',
+//   async (data={}, { rejectWithValue }) => {
+//     const query = queryString(data);
+//     try {
+//       const res = await axios.get(query);
+//       return res.data;
       
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
-  }
-);
+//     } catch (error) {
+//       return rejectWithValue(error.message);
+//     }
+//   }
+// );
 
 
 
