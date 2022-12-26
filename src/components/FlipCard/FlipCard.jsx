b5492b7e98c374a4ed74140cbb177670735c462d
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-26 22:57:07 +0200

    Create FlipCard.jsx

---
+++ src/components/FlipCard/FlipCard.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import FormTransaction from 'components/FormTransaction/FormTransaction';
+import { Card, CardBack, CardFront, InnerCard, Title } from './FlipCard.styled';
+
+const FlipCard = () => {
+  const [isIncome, setIsIncome] = useState(false);
+
+  return (
+    <Card>
+      <InnerCard isFlipped={isIncome}>
+        <CardFront>
+          <Title>Add transaction</Title>
+          {!isIncome && 
+          <FormTransaction setIsIncome={setIsIncome} isIncome={isIncome} />
+          }
+        </CardFront>
+
+        <CardBack>
+          <Title>Add transaction</Title>
+          {isIncome && 
+          <FormTransaction setIsIncome={setIsIncome} isIncome={isIncome} />
+          }
+        </CardBack>
+      </InnerCard>
+    </Card>
+  );
+};
+
+export default FlipCard;
