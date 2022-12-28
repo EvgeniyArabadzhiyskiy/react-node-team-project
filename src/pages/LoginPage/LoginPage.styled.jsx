6279aae4ea4a502e69ff584233204d6c0bae28c3
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-28 22:28:21 +0200

    Update LoginPage.styled.jsx

--- src/pages/LoginPage/LoginPage.styled.jsx
+++ src/pages/LoginPage/LoginPage.styled.jsx
@@ -1,21 +1,22 @@
 import styled from 'styled-components';
 import EllipseRed from '../../images/EllipseRed.png';
 import EllipseViolet from '../../images/EllipseViolet.png';
+import EWalletBg from '../../images/wallet-full.jpg';
 
 export const Section = styled.section`
-  @media screen and (min-width: 768px) {
+  /* @media screen and (min-width: 768px) {
     width: 100%;
     min-height: 100vh;
     background-color: ${p => p.theme.colors.loginPageBg};
     background-image: url(${EllipseViolet}), url(${EllipseRed});
     background-position: 0 100%, 100% 0;
     background-repeat: no-repeat;
-  }
+  } */
 `;
 
 export const Blur = styled.div`
-  min-height: 100vh;
-  backdrop-filter: blur(50px);
+  /* min-height: 100vh;
+  backdrop-filter: blur(50px); */
 `;
 
 export const Container = styled.div`
@@ -41,6 +42,14 @@ export const Wrapper = styled.div`
 
   @media screen and (min-width: 1280px) {
     justify-content: space-between;
+    align-items: center;
+    height: 100vh;
+    overflow: hidden;
+
+    background: url(${EWalletBg});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
   }
 `;
 
@@ -51,7 +60,7 @@ export const Frame = styled.img`
     margin-right: 40px;
   }
   @media screen and (min-width: 1280px) {
-    margin-bottom: 32px;
+    /* margin-bottom: 32px; */
   }
 `;
 
@@ -60,13 +69,22 @@ export const ImageContainer = styled.div`
     display: flex;
     justify-content: space-evenly;
     align-items: center;
-    margin-bottom: 50px;
+    /* margin-bottom: 50px; */
   }
   @media screen and (min-width: 1280px) {
-    display: flex;
+    /* display: flex;
     justify-content: center;
     align-items: center;
-    flex-direction: column;
+    flex-direction: column; */
+    /* width: 436px; */
+    width: 650px;
+
+    height: 100vh;
+    background-color: tomato;
+    background: url(${EWalletBg});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
   }
 `;
 export const Text = styled.p`
@@ -82,11 +100,12 @@ export const FormContainer = styled.div`
   @media screen and (min-width: 1280px) {
     width: auto;
     height: 100vh;
+    /* height: 800px; */
     display: flex;
     align-items: center;
     justify-content: center;
-    backdrop-filter: blur(50px);
-    background-color: ${p => p.theme.colors.secondaryBg};
-    padding: 52px 91px 52px 107px;
+    /* backdrop-filter: blur(50px); */
+    /* background-color: ${p => p.theme.colors.secondaryBg}; */
+    /* padding: 52px 91px 52px 107px; */
   }
 `;
