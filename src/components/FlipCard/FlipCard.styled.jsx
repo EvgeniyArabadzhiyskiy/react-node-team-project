db98fe5bd809df3e0538ccf982dd2a10182faa36
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-28 22:28:18 +0200

    Update FlipCard.styled.jsx

--- src/components/FlipCard/FlipCard.styled.jsx
+++ src/components/FlipCard/FlipCard.styled.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Card = styled.div`
   width: 320px;
-  height: 620px;
+  height: 600px;
   perspective: 700px;
 
   @media screen and (min-width: 768px) {
@@ -14,44 +14,27 @@ export const InnerCard = styled.div`
   position: relative;
   height: 100%;
   width: 100%;
-  transition: transform 600ms;
+  border: 10px solid rgba(255, 255, 255, 0.2);
+  transition: transform 1000ms;
   transform-style: preserve-3d;
 
   transform: ${p => p.isFlipped && 'rotateY(180deg)'};
 `;
 
 const CardFace = styled.div`
-  padding-top: ${p => p.theme.space[6]}px;
+  padding-top: ${p => p.theme.space[4]}px;
   position: absolute;
   height: 100%;
   width: 100%;
   backface-visibility: hidden;
   -webkit-backface-visibility: hidden;
   overflow: hidden;
-  border-radius: 20px;
-  background-color: #fff;
+  background: rgba(0, 0, 0, 0.7);
   transform-style: preserve-3d;
 `;
 
-export const CardFront = styled(CardFace)`
-`;
+export const CardFront = styled(CardFace)``;
 
 export const CardBack = styled(CardFace)`
   transform: rotateY(180deg);
 `;
-
-
-// export const Title = styled.h2`
-//   font-family: ${p => p.theme.fonts.heading};
-//   font-size: 24px;
-//   line-height: 35px;
-//   font-weight: ${p => p.theme.fontWeights.normal};
-//   text-align: center;
-
-//   margin-bottom: 40px;
-
-//   @media screen and (min-width: 768px) {
-//     font-size: ${p => p.theme.fontSizes.l};
-//     line-height: 45px;
-//   }
-// `;
