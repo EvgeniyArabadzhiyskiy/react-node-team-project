49b6a6f12b3724f6d70e455472c7ea71f730e757
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-30 22:57:48 +0200

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,20 +1,26 @@
 import logo from '../../images/logo.png';
 import logoBig from '../../images/logoBig.png';
-import { LogoBox, LogoLink, LogoWallet } from './Logo.styled';
+import { LogoBox, LogoLink, LogoText, LogoWallet } from './Logo.styled';
 import { useMedia } from 'react-use';
+import { useLocation } from 'react-router-dom';
 
 const Logo = () => {
-  const isWide = useMedia('(min-width: 768px)');
+  const location = useLocation();
+  const isDesctop = useMedia('(min-width: 768px)');
+
+  const isLoginPage =
+  location.pathname === '/login' || 
+  location.pathname === '/register';
 
   return (
     <LogoBox>
-      <LogoLink to="/home">
-        {isWide ? (
+      <LogoLink to={isLoginPage ? '/login' : '/home'}>
+        {isDesctop ? (
           <LogoWallet src={logoBig} alt="logo" />
         ) : (
           <LogoWallet src={logo} alt="logo" />
         )}
-        Wallet
+        <LogoText isLoginPage={isLoginPage}>Wallet</LogoText>
       </LogoLink>
     </LogoBox>
   );
