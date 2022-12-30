61984bd78216c049d604b46cdc321b9537f82352
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-30 22:58:06 +0200

    Update LoginPage.jsx

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,91 +1,23 @@
-import LoginForm from 'components/LoginForm';
-import RegistrationForm from 'components/RegistrationForm';
-import {
-  Section,
-  Container,
-  // Frame,
-  // ImageContainer,
-  FormContainer,
-  Wrapper,
-  Blur,
-  // Text,
-} from './LoginPage.styled';
-// import frame from '../../images/frame.png';
-// import frameReg from '../../images/frameReg.png';
-// import frameBig from '../../images/frameBig.png';
-// import frameRegBig from '../../images/frameRegBig.png';
-// import EWalletBg from '../../images/ewallet-5.png';
-
-import { useMedia } from 'react-use';
 import { useLocation } from 'react-router-dom';
+
+import LoginForm from 'components/LoginForm';
 import SharedLayout from 'components/SharedLayout';
+import RegistrationForm from 'components/RegistrationForm';
+import { Section, Container, Wrapper } from './LoginPage.styled';
 
 const LoginPage = () => {
-  const isWideMobie = useMedia('(max-width: 767px)');
-  const isWideTablet = useMedia('(min-width: 768px) and (max-width: 1279px)');
-  const isWideComputer = useMedia('(min-width: 1280px)');
   const location = useLocation();
 
   return (
     <SharedLayout>
       <Section>
-        {isWideMobie ? (
-          <Container>
-            <Wrapper>
-              {location.pathname === '/login' ? (
-                <LoginForm />
-              ) : (
-                <RegistrationForm />
-              )}
-            </Wrapper>
-          </Container>
-        ) : null}
-        {isWideTablet ? (
-          <Container>
-            <Wrapper>
-              {/* <ImageContainer>
-                <Frame
-                  src={location.pathname === '/login' ? frame : frameReg}
-                  alt="customer orders goods"
-                />
-                <Text>Finance App</Text>
-              </ImageContainer> */}
-              <FormContainer>
-                {location.pathname === '/login' ? (
-                  <LoginForm />
-                ) : (
-                  <RegistrationForm />
-                )}
-              </FormContainer>
-            </Wrapper>
-          </Container>
-        ) : null}
-        {isWideComputer ? (
-          <Blur>
-            <Container>
-              <Wrapper>
-                <div>
-                  {/* <Frame
-                    // src={
-                    //   location.pathname === '/login' ? frameBig : frameRegBig
-                    // }
-                    src={EWalletBg}
-                    alt="customer orders goods"
-                  /> */}
-                  {/* <Text>Finance App</Text> */}
-                </div>
-
-                <FormContainer>
-                  {location.pathname === '/login' ? (
-                    <LoginForm />
-                  ) : (
-                    <RegistrationForm />
-                  )}
-                </FormContainer>
-              </Wrapper>
-            </Container>
-          </Blur>
-        ) : null}
+        <Container>
+          <Wrapper>
+            { location.pathname === '/login' 
+            ? <LoginForm />
+            : <RegistrationForm /> }
+          </Wrapper>
+        </Container>
       </Section>
     </SharedLayout>
   );
