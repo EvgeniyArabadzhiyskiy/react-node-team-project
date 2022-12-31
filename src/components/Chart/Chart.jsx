50df2f34a305e606948ba4fe4ef954573d71adbd
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-31 18:22:02 +0200

    Update Chart.jsx

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -6,51 +6,28 @@ import {
   DoughnutController,
 } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
-import { selectAllStatistic } from 'redux/statistic/statisticSelectors';
 import { useSelector } from 'react-redux';
-import { getCategoryColor } from 'helpers/getCategoryColor';
+import { useGetStatisticQuery } from 'redux/WalletApiServise/wallet-api';
+import { getStatsResult } from 'helpers/statistics/getStatsResult';
+import { drawInnerText, options } from 'helpers/statistics/doughnutOptions';
 
 ChartJS.register(ArcElement, Tooltip, Legend, DoughnutController);
 
 const Chart = () => {
-  const res = useSelector(selectAllStatistic);
+  const { month, year } = useSelector(state => state.statistic);
   const { totalBalance } = useSelector(state => state.transactions);
+  const {data: stats = [] } = useGetStatisticQuery({ month, year })
 
-  const expenseResults = res.statistic.filter(
-    result => result.type === 'expense'
-  );
-
-  const chartCategories = expenseResults.map(result => {
-    return result._id;
-  });
-  const sum = expenseResults.map(result => result.totalSum);
-
-  const colors = expenseResults.map(item => getCategoryColor(item._id));
-
-  const emptyDiagramLable = ['empty'];
-  const emptyData = ['100'];
-
-  const drawInnerText = chart => {
-    const ctx = chart.ctx;
-    ctx.restore();
-    ctx.font = '700 18px Circe-Regular';
-    ctx.textBaseline = 'middle';
-    const text = `₴${totalBalance}`;
-    const textX = Math.round((chart.width - ctx.measureText(text).width) / 2);
-    const textY = chart.height / 2 + chart.legend.height / 2;
-    ctx.fillText(text, textX, textY);
-    ctx.fillStyle = '#000000';
-    ctx.save();
-  };
+  const { sum, colors, chartCategories } = getStatsResult(stats)
 
   const data = {
-    labels: chartCategories.length > 0 ? chartCategories : emptyDiagramLable,
+    labels: chartCategories.length > 0 ? chartCategories : ['empty'],
     datasets: [
       {
         label: '# of Votes',
-        data: sum.length > 0 ? sum : emptyData,
+        data: sum.length > 0 ? sum : ['100'],
         text: 'summ',
-        backgroundColor:
+        backgroundColor: 
           colors.length > 0 ? colors : ['rgba(255, 99, 132, 0.2)'],
         borderWidth: 0,
       },
@@ -58,29 +35,16 @@ const Chart = () => {
   };
 
   return (
-    <div style={{ maxWidth: '336px', margin: '0 auto' }}>
-      <Doughnut
-        width={336}
-        height={336}
-        data={data}
-        options={{
-          cutout: '70%',
-          plugins: {
-            legend: {
-              display: false,
-            },
-          },
-        }}
-        plugins={[
-          {
-            beforeDraw: function (chart) {
-              drawInnerText(chart);
-            },
-          },
-        ]}
-      />
-    </div>
+    <Doughnut
+      width={336}
+      height={336}
+      data={data}
+      options={options}
+      plugins={[{beforeDraw: (chart) => drawInnerText(chart, totalBalance)}]}
+    />
   );
 };
 
 export default Chart;
+
+
