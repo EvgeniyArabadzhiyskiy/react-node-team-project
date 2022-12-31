3e5c1f02f2caf75cb5eb5cf2073cdd21a0a03117
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-12-31 18:22:38 +0200

    Update transactionsSlice.js

--- src/redux/transactions/transactionsSlice.js
+++ src/redux/transactions/transactionsSlice.js
@@ -17,7 +17,7 @@ const transactionsSlice = createSlice({
       state.isModalAddOpen = action.payload;
     },
 
-    getNextPage: (state, action) => {
+    getNextPage: (state, _) => {
       state.pageNum = state.pageNum + 1;
     },
 
@@ -34,48 +34,15 @@ const transactionsSlice = createSlice({
   },
 
   // extraReducers: builder => {
-  //   builder.addCase(addNewTransaction.pending, state => {
-  //     state.error = null;
-  //     state.isLoading = true;
-  //   });
+  //   builder.addMatcher(walletsApi.endpoints.getAllTransactions.matchFulfilled,
+  //     (state, action) => {
+  //       // console.log("action", action.payload);
+  //       state.transactions = [...state.transactions, ...action.payload.transactions];
+  //       // state.totalBalance = action.payload.userBalance;
+        
+  //     })
+  // }
 
-  //   builder.addCase(addNewTransaction.fulfilled, (state, action) => {
-  //     state.isLoading = false;
-  //   });
-
-  //   builder.addCase(addNewTransaction.rejected, (state, action) => {
-  //     state.isLoading = false;
-  //     state.error = action.payload;
-  //   });
-
-  //   builder.addCase(getAllTransactions.pending, (state, _) => {
-  //     state.error = null;
-  //     state.isLoading = true;
-  //   });
-
-  //   builder.addCase(getAllTransactions.fulfilled, (state, action) => {
-  //     state.isLoading = false;
-  //     state.hasNextPage = Boolean(action.payload.transactions.length);
-
-  //     if (state.pageNum === 1) {
-  //       state.transactions = [...action.payload.transactions];
-  //     } else {
-  //       state.transactions = [
-  //         ...state.transactions,
-  //         ...action.payload.transactions,
-  //       ];
-  //     }
-
-  //     if (action.payload.transactions.length > 0 && state.pageNum === 1) {
-  //       state.totalBalance = action.payload.userBalance;
-  //     }
-  //   });
-
-  //   builder.addCase(getAllTransactions.rejected, (state, action) => {
-  //     state.isLoading = false;
-  //     state.error = action.payload;
-  //   });
-  // },
 });
 
 export const { toggleModalAdd, getNextPage, resetTransactions, getTransactions } = transactionsSlice.actions;
