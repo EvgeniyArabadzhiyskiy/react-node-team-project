85a3c2935b49b0e66236733fc29a2ca084fe62ec
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-02 22:25:15 +0200

    Update ModalLogout.jsx

--- src/components/ModalLogout/ModalLogout.jsx
+++ src/components/ModalLogout/ModalLogout.jsx
@@ -1,68 +1,53 @@
 import {
-  Overlay,
-  Modal,
-  ModalTitle,
+  Title,
   Wrapper,
   WrapperItem,
   Exit,
   Stay,
+  Border,
+  Modal,
 } from './ModalLogout.styled';
-import { enablePageScroll } from 'scroll-lock';
-import { useEffect } from 'react';
-// import { userLogout } from '../../redux/auth/authOperation';
 import { useDispatch } from 'react-redux';
 import Spinner from 'components/Spinner';
-import { resetTransactions } from 'redux/transactions/transactionsSlice';
+import {
+  resetTransactions,
+  toggleModalOverlay,
+  toggleModalExit,
+} from 'redux/transactions/transactionsSlice';
 import { useUserLogoutMutation } from 'redux/WalletApiServise/wallet-api';
 
-const ModalLogout = ({ openExitModal, setIsOpenExitModal }) => {
+const ModalLogout = () => {
   const dispatch = useDispatch();
   const [logout_RTKQ, { isLoading }] = useUserLogoutMutation();
 
-  useEffect(() => {
-    document.addEventListener('keydown', closeModalEsc);
-
-    return function () {
-      document.removeEventListener('keydown', closeModalEsc);
-    };
-  });
-
-  function closeModalEsc(e) {
-    if (e.key === 'Escape') {
-      setIsOpenExitModal(false);
-    }
-  }
-
-  const disableScrollOn = () => {
-    enablePageScroll();
-    setIsOpenExitModal(false);
+  const onCancelClick = () => {
+    dispatch(toggleModalOverlay(false));
+    dispatch(toggleModalExit(false));
   };
 
   const logOut = async () => {
-    // await  dispatch(userLogout());
+    await logout_RTKQ();
+    await dispatch(resetTransactions());
 
-    await logout_RTKQ()
-    await  dispatch(resetTransactions())
-
-    enablePageScroll();
-    setIsOpenExitModal(false);
+    dispatch(toggleModalOverlay(false));
+    dispatch(toggleModalExit(false));
   };
 
   return (
-    <Overlay onClick={disableScrollOn} opened={openExitModal}>
-      <Modal onClick={e => e.stopPropagation()} opened={openExitModal}>
-        {isLoading && <Spinner />}
-        <ModalTitle>Are you sure you want to sign out?</ModalTitle>
-        <Wrapper>
-          <WrapperItem>
-            <Exit onClick={logOut}>Exit </Exit>
-          </WrapperItem>
-          <WrapperItem>
-            <Stay onClick={disableScrollOn}>Cancel</Stay>
-          </WrapperItem>
-        </Wrapper>
+    <Border>
+      {isLoading && <Spinner />}
+      <Modal>
+      <Title>Are you sure you want to sign out?</Title>
+      <Wrapper>
+        <WrapperItem>
+          <Exit onClick={logOut}>Exit </Exit>
+        </WrapperItem>
+        <WrapperItem>
+          <Stay onClick={onCancelClick}>Cancel</Stay>
+        </WrapperItem>
+      </Wrapper>
       </Modal>
-    </Overlay>
+    </Border>
   );
 };
 
