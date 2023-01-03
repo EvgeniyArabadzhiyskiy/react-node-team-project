11125302eb6c9cd3be9fe4dd65713e64f21af60f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-03 11:35:29 +0200

    Update ModalLogout.jsx

--- src/components/ModalLogout/ModalLogout.jsx
+++ src/components/ModalLogout/ModalLogout.jsx
@@ -9,11 +9,9 @@ import {
 } from './ModalLogout.styled';
 import { useDispatch } from 'react-redux';
 import Spinner from 'components/Spinner';
-import {
-  resetTransactions,
-  toggleModalOverlay,
-  toggleModalExit,
-} from 'redux/transactions/transactionsSlice';
+
+import { modalClose } from 'redux/modal/modalSlice';
+import { resetTransactions } from 'redux/transactions/transactionsSlice';
 import { useUserLogoutMutation } from 'redux/WalletApiServise/wallet-api';
 
 const ModalLogout = () => {
@@ -21,31 +19,29 @@ const ModalLogout = () => {
   const [logout_RTKQ, { isLoading }] = useUserLogoutMutation();
 
   const onCancelClick = () => {
-    dispatch(toggleModalOverlay(false));
-    dispatch(toggleModalExit(false));
+    dispatch(modalClose(false));
   };
 
   const logOut = async () => {
     await logout_RTKQ();
-    await dispatch(resetTransactions());
 
-    dispatch(toggleModalOverlay(false));
-    dispatch(toggleModalExit(false));
+    dispatch(resetTransactions());
+    dispatch(modalClose(false));
   };
 
   return (
     <Border>
       {isLoading && <Spinner />}
       <Modal>
-      <Title>Are you sure you want to sign out?</Title>
-      <Wrapper>
-        <WrapperItem>
-          <Exit onClick={logOut}>Exit </Exit>
-        </WrapperItem>
-        <WrapperItem>
-          <Stay onClick={onCancelClick}>Cancel</Stay>
-        </WrapperItem>
-      </Wrapper>
+        <Title>Are you sure you want to sign out?</Title>
+        <Wrapper>
+          <WrapperItem>
+            <Exit onClick={logOut}>Exit </Exit>
+          </WrapperItem>
+          <WrapperItem>
+            <Stay onClick={onCancelClick}>Cancel</Stay>
+          </WrapperItem>
+        </Wrapper>
       </Modal>
     </Border>
   );
