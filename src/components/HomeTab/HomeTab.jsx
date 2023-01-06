3ba5fb8dd552f1b14316a45dcfb087752befe6df
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-06 20:50:13 +0200

    Update HomeTab.jsx

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -1,6 +1,6 @@
-import { forwardRef } from 'react';
+import {  useCallback, useEffect, useRef } from 'react';
 import { useMedia } from 'react-use';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { HomeTabItem, HomeTabMobItem } from './HomeTabItem';
 import {
@@ -12,25 +12,80 @@ import {
 } from './HomeTab.styled';
 import { getBalances } from 'helpers/formAddTransaction/getBalance';
 import ButtonAddTransactions from 'components/ButtonAddTransactions';
+import { useGetAllTransactionsQuery } from 'redux/WalletApiServise/wallet-api';
+import { getNextPage, getTransactions, setUnmount } from 'redux/transactions/transactionsSlice';
 
-const HomeTab = forwardRef(({ data }, ref) => {
-  const isMobie = useMedia('(max-width: 767px)');
-  const { totalBalance } = useSelector(state => state.transactions);
 
-  const balances = getBalances(data, totalBalance);
+const HomeTab = () => {
+  const isMobile = useMedia('(max-width: 767px)');
+  const isDesctop = useMedia('(min-width: 768px)');
+  // const { totalBalance } = useSelector(state => state.transactions);
+  
+  const dispatch = useDispatch();
+  const { transactions, pageNum } = useSelector(state => state.transactions);
+  console.log("HomeTab  transactions", transactions);
+  const { data = {} } = useGetAllTransactionsQuery(pageNum)
+  // console.log("HomeTab  data", data.userBalance);
+    
+   
+
+
+  useEffect(() => {
+    return () => {
+      console.log('Component Unmount');
+      dispatch(setUnmount(true));
+    }
+  },[dispatch]);
+
+  useEffect(() => {
+    if(!data.transactions) return
+    dispatch(getTransactions(data));
+    
+  },[data, dispatch]);
+
+ 
+
+  const observer = useRef(null);
+
+  const lastElement = useCallback(item => {
+
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting ) {
+        dispatch(setUnmount(false));
+
+        // console.log('ОЧИСТИЛИ НАБЛЮДЕНИЕ :', entries[0].target,);
+
+        observer.current.unobserve(entries[0].target);
+        dispatch(getNextPage());
+      }
+      }, { rootMargin: '5px',threshold: 0.95});
+
+      if (item)  {
+        // console.log('НАБЛЮДЕНИЕ ВЕДЕТСЯ ЗА :', item);
+        observer.current.observe(item);
+      }
+    },[dispatch]
+  );
+
+
+
+
+
+  const balances = getBalances(transactions, data.userBalance);
 
   return (
     <div>
-      {isMobie ? (
+      { isMobile  &&
         <StyledWrap>
-          {data.map(
+          {transactions.map(
             ({ _id, date, typeOperation, category, comment, amount }, idx) => {
               const itemBalance = balances[idx];
 
-              if (data.length === idx + 1) {
+              if (transactions.length === idx + 1) {
                 return (
+                  // transactions.length > 0 &&
                   <HomeTabMobItem
-                    ref={ref}
+                    ref={lastElement}
                     key={_id}
                     transaction={{_id, date, typeOperation, category, comment, amount, itemBalance }}
                   />
@@ -38,6 +93,7 @@ const HomeTab = forwardRef(({ data }, ref) => {
               }
 
               return (
+                // transactions.length > 0 &&
                 <HomeTabMobItem
                   key={_id}
                   transaction={{ _id, date, typeOperation, category, comment, amount, itemBalance }}
@@ -45,7 +101,8 @@ const HomeTab = forwardRef(({ data }, ref) => {
               )}
           )}
         </StyledWrap>
-      ) : (
+      }
+       { isDesctop  &&
         <StyledTable>
           <StyledTableHeader>
             <CategoryName>Date</CategoryName>
@@ -56,15 +113,15 @@ const HomeTab = forwardRef(({ data }, ref) => {
             <CategoryName>Balance</CategoryName>
           </StyledTableHeader>
 
-          { data.length > 0 && <StyledTableBody>
-            {data.map(({ _id, date, typeOperation, category, comment, amount }, idx ) => {
+          { transactions.length > 0 && <StyledTableBody>
+            {transactions.map(({ _id, date, typeOperation, category, comment, amount }, idx ) => {
               const itemBalance = balances[idx];
 
-              if (data.length === idx + 1) {
+              if (transactions.length === idx + 1) {
                 return (
                   <HomeTabItem
                     key={_id}
-                    ref={ref}
+                    ref={lastElement}
                     transaction={{ _id, date, typeOperation, category, comment, amount, itemBalance }}
                   />
                 )
@@ -78,12 +135,56 @@ const HomeTab = forwardRef(({ data }, ref) => {
               )}
             )}
           </StyledTableBody>}
-        </StyledTable>
-      )}
+        </StyledTable>}
+      
       
       <ButtonAddTransactions />
     </div>
   );
-});
+};
 
 export default HomeTab;
+
+
+
+
+// const dispatch = useDispatch();
+// const { transactions, pageNum } = useSelector(state => state.transactions);
+// const { data = {} } = useGetAllTransactionsQuery(pageNum)
+  
+// useEffect(() => {
+//   return () => {
+//     console.log('Component Unmount');
+//     dispatch(setUnmount(true));
+//   }
+// },[dispatch]);
+
+// useEffect(() => {
+//   if(!data.transactions) return
+//   dispatch(getTransactions(data));
+  
+// },[data, dispatch]);
+
+
+
+// const observer = useRef(null);
+
+// const lastElement = useCallback(item => {
+
+//   observer.current = new IntersectionObserver(entries => {
+//     if (entries[0].isIntersecting ) {
+//       dispatch(setUnmount(false));
+
+//       // console.log('ОЧИСТИЛИ НАБЛЮДЕНИЕ :', entries[0].target,);
+
+//       observer.current.unobserve(entries[0].target);
+//       dispatch(getNextPage());
+//     }
+//     }, { rootMargin: '5px',threshold: 0.95});
+
+//     if (item)  {
+//       // console.log('НАБЛЮДЕНИЕ ВЕДЕТСЯ ЗА :', item);
+//       observer.current.observe(item);
+//     }
+//   },[dispatch]
+// );
