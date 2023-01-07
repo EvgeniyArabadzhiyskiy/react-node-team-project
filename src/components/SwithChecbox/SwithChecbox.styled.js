30fa5d219e707919dd12f20344ea06145a3465a8
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-07 21:59:07 +0200

    Create SwithChecbox.styled.js

---
+++ src/components/SwithChecbox/SwithChecbox.styled.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+import { Field } from 'formik';
+
+export const CheckBoxWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: ${p => p.theme.space[4]}px;
+  margin-bottom: ${p => p.theme.space[7]}px;
+
+  font-size: ${p => p.theme.fontSizes.s};
+  line-height: 24px;
+  font-weight: 600;
+`;
+
+export const CheckBoxLabel = styled.label`
+  position: relative;
+  width: 80px;
+  height: 40px;
+  border: 1px solid #e0e0e0;
+  border-radius: 30px;
+`;
+
+export const CheckBox = styled(Field)`
+  width: ${p => p.theme.space[4]}px;
+  height: ${p => p.theme.space[4]}px;
+  visibility: hidden;
+`;
+
+export const Switch = styled.p`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: absolute;
+  top: -2px;
+  left: -2px;
+
+  width: 44px;
+  height: 44px;
+  border-radius: ${p => p.theme.radii.round};
+  background-color: ${p => p.theme.colors.expense};
+  transition: transform 300ms linear;
+
+  background-color: ${prop => (prop.isChecked ? '#24CCA7' : '#FF6596')};
+  transform: ${prop => (prop.isChecked ? 'translateX(0)' : 'translateX(40px)')};
+`;
+
+export const TextIncome = styled.p`
+  color: ${prop => (prop.isChecked ? '#24CCA7' : '#e0e0e0')};
+`;
+
+export const TextExpense = styled.p`
+  color: ${prop => (prop.isChecked ? '#e0e0e0' : '#ff6596')};
+`;
