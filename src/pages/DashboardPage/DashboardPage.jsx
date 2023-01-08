f01a5b4ffc6e7f596823252495246411896acf22
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-08 13:52:03 +0200

    Update DashboardPage.jsx

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,4 +1,3 @@
-// import SharedLayout from 'components/SharedLayout';
 import Currency from 'components/Currency';
 import Header from 'components/Header';
 import Navigation from 'components/Navigation';
@@ -10,15 +9,12 @@ import bgImg from '../../images/Rectangle.png';
 import {
   Section,
   Container,
-  Wrapper,
   SideBar,
   DesctopWrapper,
 } from './Dashboard.styled';
 
 const DashboardPage = () => {
   const isMobie = useMedia('(max-width: 767px)');
-  // const isTablet = useMedia('(min-width: 768px) and (max-width: 1279px)');
-  // const isDesktop = useMedia('(min-width: 1280px)');
 
   return (
     <>
@@ -26,14 +22,13 @@ const DashboardPage = () => {
       <Section bgImg={bgImg}>
         <Container>
           <DesctopWrapper>
-            <Wrapper>
-              <SideBar>
+            <SideBar>
+              <div>
                 <Navigation />
                 <Balance />
-                {!isMobie && <Currency />}
-              </SideBar>
-            </Wrapper>
-
+              </div>
+              {!isMobie && <Currency />}
+            </SideBar>
             <Suspense fallback={null}>
               <Outlet />
             </Suspense>
@@ -45,60 +40,3 @@ const DashboardPage = () => {
 };
 
 export default DashboardPage;
-
-// <SharedLayout>
-//   <Header />
-//   <main>
-//     <Section bgImg={bgImg}>
-//       {isMobie ? (
-//         <Blur>
-//           <Container>
-//             <Wrapper>
-//               <Navigation />
-//               <Balance />
-//               <Suspense fallback={null}>
-//                 <Outlet />
-//               </Suspense>
-//             </Wrapper>
-//           </Container>
-//         </Blur>
-//       ) : null}
-//       {isTablet ? (
-//         <Blur>
-//           <Container>
-//             <Wrapper>
-//               <TabletWrapper>
-//                 <Navigation />
-//                 <Balance />
-//               </TabletWrapper>
-//               <Currency />
-//             </Wrapper>
-//             <div>
-//               <Suspense fallback={null}>
-//                 <Outlet />
-//               </Suspense>
-//             </div>
-//           </Container>
-//         </Blur>
-//       ) : null}
-//       {isDesktop ? (
-//         <Blur>
-//           <Container>
-//             <Wrapper>
-//               <SideBar>
-//                 <Navigation />
-//                 <Balance />
-//                 <Currency />
-//               </SideBar>
-//               <div>
-//                 <Suspense fallback={null}>
-//                   <Outlet />
-//                 </Suspense>
-//               </div>
-//             </Wrapper>
-//           </Container>
-//         </Blur>
-//       ) : null}
-//     </Section>
-//   </main>
-// </SharedLayout>
