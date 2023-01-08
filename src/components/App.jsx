9bb053baaba3132d9cd8016ea5cc7cc4e941def0
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-08 13:51:26 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,14 +17,10 @@ import ModalLogout from './ModalLogout';
 import ModalWindow from './ModalWindow/ModalWindow';
 import { nightTheme, dayTheme } from '../theme';
 import { setToken } from 'redux/auth/authSlice';
-// import { getNextPage, getTransactions } from 'redux/transactions/transactionsSlice';
-import { 
-  // useGetAllTransactionsQuery,
-   useUserRefreshQuery } from 'redux/WalletApiServise/wallet-api';
+import { useUserRefreshQuery } from 'redux/WalletApiServise/wallet-api';
 import { AnimatePresence } from 'framer-motion';
 import { GlobalStyle } from './GlobalStyle';
 // import Tester from './Tester/Tester';
-
 
 
 const LoginPage = lazy(() => import('../pages/LoginPage'));
@@ -38,15 +34,11 @@ export const App = () => {
   const isMobie = useMedia('(max-width: 767px)');
   const [searchParams] = useSearchParams();
 
-
-
-  const isDarkTheme = useSelector(store => store.theme.isNightTheme);
   const { token } = useSelector(state => state.auth);
-  // const { transactions, pageNum, isUnmount } = useSelector(state => state.transactions);
+  const { isNightTheme } = useSelector(store => store.theme);
   const {  isModalOpen, modalExit, modalTransaction } = useSelector(state => state.modal);
 
   const { isError, isLoading } = useUserRefreshQuery(undefined, { skip: !token })
-  // const { data = {} } = useGetAllTransactionsQuery(pageNum, { skip: !token && isUnmount })
   
   useEffect(() => {
     const accessToken = searchParams.get('accessToken');
@@ -55,92 +47,55 @@ export const App = () => {
     dispatch(setToken(accessToken))
   },[dispatch, searchParams])
 
-  // useEffect(() => {
-  //   if(!data.transactions) return
-  //   // console.log("useEffect  data.transactions", data.transactions);
-
-  //   setTransactions(prev => [...prev, ...data.transactions]);
-  // }, [data]);
- 
-  // useEffect(() => {
-  //   if(!data.transactions) return
-
-  //   dispatch(getTransactions(data));
-  // }, [data, dispatch]);
-
-  // const observer = useRef(null);
-
-  // const lastElement = useCallback(item => {
-
-  //   observer.current = new IntersectionObserver(entries => {
-  //     if (entries[0].isIntersecting ) {
-  //       // console.log('ОЧИСТИЛИ НАБЛЮДЕНИЕ :', entries[0].target,);
-
-  //       observer.current.unobserve(entries[0].target);
-  //       dispatch(getNextPage());
-  //     }
-  //     }, { rootMargin: '5px',threshold: 0.95});
-
-  //     if (item)  {
-  //       // console.log('НАБЛЮДЕНИЕ ВЕДЕТСЯ ЗА :', item);
-  //       observer.current.observe(item);
-  //     }
-  //   },[dispatch]
-  // );
-
   if (isError)  toast.error("Error")
   
-  return isLoading ? (
-    <Spinner />
-  ) : (
-    <ThemeProvider theme={isDarkTheme ? dayTheme : nightTheme}>
-      <Suspense fallback={null}>
-        <Routes>
+  return isLoading 
+    ? <Spinner /> 
+    : <ThemeProvider theme={isNightTheme ? dayTheme : nightTheme}>
+        <Suspense fallback={null}>
+          <Routes>
+            
+            <Route
+              path="/login"
+              element={
+                <PublicRoute restricted navigateTo="/home">
+                  <LoginPage />
+                </PublicRoute>}
+            />
+
+            <Route
+              path="/register"
+              element={
+                <PublicRoute restricted navigateTo="/home">
+                  <LoginPage />
+                </PublicRoute>}
+            />
+
+            <Route path="/" element={<PrivateRoute><DashboardPage /></PrivateRoute>}>
+              <Route index element={<Navigate to="/home" />} />
+              <Route path="home" element={<HomeTab />}/>
+              <Route path="statistic" element={<DiagramTab />} />
+              {isMobie &&  <Route path="currency" element={<Currency /> }/>} 
+            </Route>
+
+            {/* <Route  path="/tester" element={<Tester />} /> */}
           
-          <Route
-            path="/login"
-            element={
-              <PublicRoute restricted navigateTo="/home">
-                <LoginPage />
-              </PublicRoute>}
-          />
 
-          <Route
-            path="/register"
-            element={
-              <PublicRoute restricted navigateTo="/home">
-                <LoginPage />
-              </PublicRoute>}
-          />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
 
-          <Route path="/" element={<PrivateRoute><DashboardPage /></PrivateRoute>}>
-            <Route index element={<Navigate to="/home" />} />
-            <Route path="home" element={<HomeTab
-            //  data={transactions} ref={lastElement}
-              />}/>
-            <Route path="statistic" element={<DiagramTab />} />
-            {isMobie &&  <Route path="currency" element={<Currency /> }/>} 
-          </Route>
-
-          {/* <Route  path="/tester" element={<Tester />} /> */}
+        <AnimatePresence>
+          {isModalOpen && (
+            <ModalWindow>
+              {modalTransaction && <FlipCard />}
+              {modalExit && <ModalLogout />}
+            </ModalWindow>)}
+        </AnimatePresence>
         
-
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Suspense>
-
-      <AnimatePresence>
-        {isModalOpen && (
-          <ModalWindow>
-            {modalTransaction && <FlipCard />}
-            {modalExit && <ModalLogout />}
-          </ModalWindow>)}
-      </AnimatePresence>
-      
-      <GlobalStyle />
-      <ToastContainer autoClose={2500} theme="colored" />
-    </ThemeProvider>
-  );
+        <GlobalStyle />
+        <ToastContainer autoClose={2500} theme="colored" />
+      </ThemeProvider>
 };
 
 
