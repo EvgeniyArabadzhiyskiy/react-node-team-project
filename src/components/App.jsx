cb2f3202c17464d35f4e906723fe02c3cdae9f5d
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-11 20:37:46 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,26 @@
-import { useMedia } from 'react-use';
+import { useEffect } from 'react';
+import {  useSearchParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect,  lazy, Suspense } from 'react';
-import { Navigate, Route, Routes, useLocation, useSearchParams } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import HomeTab from './HomeTab';
+import Router from './Router';
 import Spinner from './Spinner';
-import Currency from './Currency';
-import DiagramTab from './DiagramTab';
-import PublicRoute from './PublicRoute';
-import PrivateRoute from './PrivateRoute';
-import FlipCard from './FlipCard/FlipCard';
+import FlipCard from './FlipCard';
 import ModalLogout from './ModalLogout';
-import ModalWindow from './ModalWindow/ModalWindow';
+import ModalWindow from './ModalWindow';
+
+import { GlobalStyle } from './GlobalStyle';
 import { nightTheme, dayTheme } from '../theme';
 import { setToken } from 'redux/auth/authSlice';
-import { useUserRefreshQuery } from 'redux/WalletApiServise/wallet-api';
 import { AnimatePresence } from 'framer-motion';
-import { GlobalStyle } from './GlobalStyle';
-import { getPath } from 'helpers/getPath';
-
-
-const LoginPage = lazy(() => import('../pages/LoginPage'));
-const DashboardPage = lazy(() => import('../pages/DashboardPage'));
-const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
+import { useUserRefreshQuery } from 'redux/WalletApiServise/wallet-api';
 
 // user1000@mail.com
 
 export const App = () => {
   const dispatch = useDispatch();
-  const { pathname } = useLocation()
-  const isMobile = useMedia('(max-width: 767px)');
   const [searchParams] = useSearchParams();
 
   const { token } = useSelector(state => state.auth);
@@ -53,30 +41,8 @@ export const App = () => {
   return isLoading 
     ? <Spinner /> 
     : <ThemeProvider theme={isNightTheme ? dayTheme : nightTheme}>
-        <Suspense fallback={null}>
-          <Routes>
-            
-            <Route
-              path={getPath(pathname)}
-              element={
-                <PublicRoute restricted navigateTo="/home">
-                  <LoginPage />
-                </PublicRoute>}
-            />
-
-            {/* <Route  path="/tester" element={<Tester />} /> */}
-
-            <Route path="/" element={<PrivateRoute><DashboardPage /></PrivateRoute>}>
-              <Route index element={<Navigate to="/home" />} />
-              <Route path="home" element={<HomeTab />}/>
-              <Route path="statistic" element={<DiagramTab />} />
-              {isMobile &&  <Route path="currency" element={<Currency /> }/>} 
-            </Route>
-
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </Suspense>
-
+        <Router />
+    
         <AnimatePresence>
           {isModalOpen && (
             <ModalWindow>
