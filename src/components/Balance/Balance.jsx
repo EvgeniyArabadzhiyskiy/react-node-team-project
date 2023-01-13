bbf8d6d791f622e282a82518df978765145983e4
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-13 08:11:42 +0200

    Update Balance.jsx

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,21 +1,15 @@
-// import { useSelector } from 'react-redux';
-import { useGetAllTransactionsQuery } from 'redux/WalletApiServise/wallet-api';
+import { useGetBalanceQuery } from 'redux/walletsApiServise/wallet-api';
 import { Container, Title, CurrencySign, Text } from './Balance.styled';
 
 const Balance = () => {
-  // const { totalBalance } = useSelector(state => state.transactions);
+  const { data = {} } = useGetBalanceQuery();
+  const userBalance = data.userBalance || 0;
 
-
-  const { data: totalBalance = 0 } = useGetAllTransactionsQuery(1, {
-    selectFromResult: ({ data }) => ({ data: data?.userBalance }),
-  });
-
-  
   return (
     <Container>
       <Title>Your balance</Title>
       <CurrencySign>₴ </CurrencySign>
-      <Text>{totalBalance}</Text>
+      <Text>{userBalance}</Text>
     </Container>
   );
 };
