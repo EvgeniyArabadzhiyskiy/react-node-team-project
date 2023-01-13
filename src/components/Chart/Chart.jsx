85a17ce501b09f75cabd289d3b36cdf37f550678
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-13 08:11:36 +0200

    Update Chart.jsx

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -7,7 +7,7 @@ import {
 } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
 import { useSelector } from 'react-redux';
-import { useGetAllTransactionsQuery, useGetStatisticQuery } from 'redux/WalletApiServise/wallet-api';
+import { useGetBalanceQuery, useGetStatisticQuery } from 'redux/walletsApiServise/wallet-api';
 import { getStatsResult } from 'helpers/statistics/getStatsResult';
 import { drawInnerText, options } from 'helpers/statistics/doughnutOptions';
 
@@ -15,13 +15,12 @@ ChartJS.register(ArcElement, Tooltip, Legend, DoughnutController);
 
 const Chart = () => {
   const { month, year } = useSelector(state => state.statistic);
-  // const { totalBalance } = useSelector(state => state.transactions);
+  
   const {data: stats = [] } = useGetStatisticQuery({ month, year })
 
-  const { data: totalBalance = 0 } = useGetAllTransactionsQuery(1, {
-    selectFromResult: ({ data }) => ({ data: data?.userBalance }),
-  });
- 
+  const { data: balance = {} } = useGetBalanceQuery();
+  const totalBalance = balance.userBalance || 0;
+
   const { sum, colors, chartCategories } = getStatsResult(stats)
 
   const data = {
