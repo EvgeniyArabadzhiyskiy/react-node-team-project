258917d39d4de5d58e2291a5b1cdf3c82600c830
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-15 14:52:28 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,4 @@
-import { useEffect } from 'react';
-import {  useSearchParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -13,16 +11,13 @@ import ModalWindow from './ModalWindow';
 
 import { GlobalStyle } from './GlobalStyle';
 import { nightTheme, dayTheme } from '../theme';
-import { setToken } from 'redux/auth/authSlice';
 import { AnimatePresence } from 'framer-motion';
 import { useGetBalanceQuery, useUserRefreshQuery } from 'redux/walletsApiServise/wallet-api';
+import { useGoogleAuth } from 'hooks/useGoogleAuth';
 
 // user100@mail.com
 
 export const App = () => {
-  const dispatch = useDispatch();
-  const [searchParams] = useSearchParams();
-
   const { token } = useSelector(state => state.auth);
   const { isNightTheme } = useSelector(store => store.theme);
   const {  isModalOpen, modalExit, modalTransaction } = useSelector(state => state.modal);
@@ -30,19 +25,15 @@ export const App = () => {
   const { isError, isLoading } = useUserRefreshQuery(undefined, { skip: !token })
 
   useGetBalanceQuery(undefined, { skip: !token })
-  
-  useEffect(() => {
-    const accessToken = searchParams.get('accessToken');
-    if (!accessToken) return
-    
-    dispatch(setToken(accessToken))
-  },[dispatch, searchParams])
+
+  useGoogleAuth()
 
   if (isError)  toast.error("Error")
   
   return isLoading 
     ? <Spinner /> 
     : <ThemeProvider theme={isNightTheme ? dayTheme : nightTheme}>
+      
         <Routing  />
     
         <AnimatePresence>
