492bfa3dfbab5bebafa9db19300c466596cf7bb4
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-15 20:02:31 +0200

    Create apiPath.js

---
+++ src/constants/apiPath.js
@@ -0,0 +1,10 @@
+export const BASE_URL = 'https://wallet-backend-xmk0.onrender.com/api';
+
+export const AUTH_GOOGLE = '/auth-google/google';
+export const USER_CURRENT = '/users/current';
+export const USER_REGISTER = '/users/register';
+export const USER_LOGIN = '/users/login';
+export const USER_LOGOUT = '/users/logout';
+
+export const TRANSACTIONS = '/transactions';
+export const BALANCE = '/transactions/balance';
