5fbbfe2b665d4fee2571c162adbb14a822fe924a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-17 20:58:51 +0200

    Update DateInput.styled.js

--- src/components/FormTransaction/DateInput.styled.js
+++ src/components/FormTransaction/DateInput.styled.js
@@ -1,17 +1,19 @@
 import styled from 'styled-components';
 
 export const StyledInput = styled.input`
-  width: 100%;
+  width: 120px;
+  /* width: 100%; */
+  padding-left: 20px;
   border: ${p => p.theme.borders.none};
   outline: none;
   background-color: inherit;
-  color: ${p => p.theme.colors.white};;
+  color: ${p => p.theme.colors.white};
   cursor: pointer;
 `;
 
 export const IconWrapper = styled.div`
   display: flex;
-  border-bottom: 1px solid #bdbdbd;
+  border-bottom: 1px solid #bdbdbd; 
   justify-content: space-between;
 `;
 
