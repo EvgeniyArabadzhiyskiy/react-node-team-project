7e4a657aa029a903da4dd708f8276624806a78cb
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-18 13:55:35 +0200

    Create FormInput.styled.jsx

---
+++ src/components/FormInput/FormInput.styled.jsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { Field, ErrorMessage } from 'formik';
+
+export const Label = styled.label`
+  position: relative;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid ${p => p.theme.colors.secondaryLightText};
+
+  padding-left: 10px;
+`;
+
+export const Input = styled(Field)`
+  width: 100%;
+
+  padding: 5px 20px;
+  border: ${p => p.theme.borders.none};
+  outline: none;
+  background-color: inherit;
+  color: ${p => p.theme.colors.white};
+
+  ::placeholder {
+    color: ${p => p.theme.colors.secondaryText};
+  }
+`;
+
+export const ErrorText = styled(ErrorMessage)`
+  position: absolute;
+  top: -20px;
+  left: 2px;
+  font-size: 16px;
+  letter-spacing: 0.05em;
+  color: #29ffd0;
+`;
