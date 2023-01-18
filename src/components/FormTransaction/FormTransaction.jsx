ad959022d29558a59d9f3ec9b848409eed8624e4
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-18 21:40:10 +0200

    Update FormTransaction.jsx

--- src/components/FormTransaction/FormTransaction.jsx
+++ src/components/FormTransaction/FormTransaction.jsx
@@ -1,148 +1,84 @@
 import './rdt-styles.css';
-import moment from 'moment';
+import moment from "moment";
 import Select from 'react-select';
 import Datetime from 'react-datetime';
-import { Form, Formik } from 'formik';
-import { toast } from 'react-toastify';
-import { useState, useRef } from 'react';
-import { useDispatch } from 'react-redux';
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { modalClose } from "redux/modal/modalSlice";
 
-import { modalClose } from 'redux/modal/modalSlice';
-import { resetTransactions } from 'redux/transactions/transactionsSlice';
-import { optionsExpense, optionsIncome } from 'helpers/formAddTransaction/options';
+import { Box } from "components/Box";
+import SwithChecbox from "components/SwithChecbox";
+import FormInput from "components/FormInput";
+import DateInput from "components/TransactionForm/DateInput";
+import { ErrorText } from "components/FormInput/FormInput.styled";
+import { ButtonAdd, ButtonCancel } from "components/Buttons/Buttons.styled";
+import { DateWrapper, SumWrapper } from "components/TransactionForm/TransactionForm.styled";
 
-import {
-  DateWrapper,
-  ErrorText,
-  // ImputsWrapper,
-  // InputComment,
-  // InputSum,
-  // LabelComment,
-  // LabelSum,
-  // TransactionForm,
-  SumWrapper,
-  Title,
-} from './FormTransaction.styled';
-import DateInput from './DateInput';
-import { selectStyles } from 'helpers/formAddTransaction/selectStyles';
-import { transactionShema } from 'helpers/formAddTransaction/transactionShema';
-import { checksFutureDate } from 'helpers/formAddTransaction/checksFutureDate';
-import { useAddTransactionMutation } from 'redux/walletsApiServise/wallet-api';
-import { ButtonAdd, ButtonCancel } from 'components/Buttons/Buttons.styled';
-import SwithChecbox from 'components/SwithChecbox/SwithChecbox';
-import { Box } from 'components/Box';
-import FormInput from 'components/FormInput/FormInput';
-import { onChangeType } from 'helpers/formAddTransaction/onChangeType';
+import { optionsExpense, optionsIncome } from "helpers/formAddTransaction/options";
+import { selectStyles } from "helpers/formAddTransaction/selectStyles";
+import { checksFutureDate } from "helpers/formAddTransaction/checksFutureDate";
 
-const FormTransaction = ({setIsIncome, isIncome}) => {
-  const dispatch = useDispatch();
-  const selectInputRef = useRef();
+const FormTransaction = ({data}) => {
+    const {setIsIncome, isIncome, setFieldValue, isSubmitting, submitText} = data
+    // console.log("FormTransaction  isSubmitting", isSubmitting);
 
-  const [isNextOperations, setIsNextOperations] = useState(true);
-  const [addNewTransaction_RTKQ] = useAddTransactionMutation();
 
-  const currentDate = moment().format('DD.MM.YYYY');
+    const dispatch = useDispatch();
+    const selectInputRef = useRef();
 
-  const onChangeSwitch = e => {
-    setIsIncome(e.target.checked);
-    selectInputRef.current.clearValue();
- 
-  };
+    const currentDate = moment().format('DD.MM.YYYY');
 
-  const onCancelClick = () => {
-    dispatch(modalClose(false));
-  };
-
-  const initialValues = {
-    comment: '',
-    amount: '',
-    category: '',
-    date: new Date().toString(),
-  };
-
-  const onSubmitFormTransaction = async (values, {resetForm}) => {
-    const typeOperation = onChangeType(isIncome);
-
-    const transaction = {
-      ...values,
-      amount: Number(values.amount),
-      typeOperation,
+    const onChangeSwitch = e => {
+        setIsIncome(e.target.checked);
+        selectInputRef.current.clearValue();
+     
     };
-    
-    if (isNextOperations) {
-      setIsNextOperations(false);
-      
-      await addNewTransaction_RTKQ(transaction).unwrap();
-      resetForm();
 
-      dispatch(resetTransactions());
-      dispatch(modalClose(false));
+    const onCancelClick = () => {
+        dispatch(modalClose(false));
+      };
 
-      toast.success("Successful transaction");
-    }
+    return <>
+        <SwithChecbox isIncome={isIncome} onChangeSwitch={onChangeSwitch} />
 
-  };
+        <Box position="relative" marginBottom="28px">
+            <Select
+                ref={selectInputRef}
+                name="category"
+                options={ isIncome ? optionsIncome : optionsExpense}
+                placeholder="Select a category"
+                onChange={data => setFieldValue('category', data?.label)}
+                styles={selectStyles}
+            />
+            <ErrorText component="div" name="category" />
+        </Box>
 
-  return (
-    <>
-      <Title>Add transaction</Title>
-      
-      <Formik
-        initialValues={initialValues}
-        onSubmit={onSubmitFormTransaction}
-        validationSchema={transactionShema}
-      >
-        {({ setFieldValue, values }) => (
-         <Form autoComplete="off">
-            <>
-
-              <SwithChecbox isIncome={isIncome} onChangeSwitch={onChangeSwitch} />
-
-              <Box position="relative" marginBottom="28px">
-                <Select
-                  ref={selectInputRef}
-                  name="category"
-                  options={ isIncome ? optionsIncome : optionsExpense}
-                  placeholder="Select a category"
-                  onChange={data => setFieldValue('category', data?.label)}
-                  styles={selectStyles}
-                />
-                <ErrorText component="div" name="category" />
-              </Box>
-              
-              <DateWrapper>
-                <SumWrapper>
-                  <FormInput type="number"  name="amount" placeholder="0.00" />
-                </SumWrapper>
-                
-                <Datetime
-                  name="date"
-                  closeOnSelect
-                  initialValue={currentDate}
-                  dateFormat="DD.MM.YYYY"
-                  timeFormat={false}
-                  isValidDate={checksFutureDate}
-                  onChange={e => setFieldValue('date', new Date(e).toString())}
-                  inputProps={{onKeyDown: e => e.preventDefault()}}
-                  renderInput={(p, openCalendar) => (
-                  <DateInput  props={p} onOpen={openCalendar}  />)}
-                />
-              </DateWrapper>
-
-              <Box marginBottom="0">
-                <FormInput name="comment" placeholder="Comment" />
-              </Box>
-
-            </>
-
-            <ButtonAdd type="submit">Add</ButtonAdd>
-            <ButtonCancel type="button" onClick={onCancelClick}>Cancel</ButtonCancel>
+        <DateWrapper>
+            <SumWrapper>
+                <FormInput type="number"  name="amount" placeholder="0.00" />
+            </SumWrapper>
             
-          </Form>
-        )}
-      </Formik> 
-    </>
-  );
-};
+            <Datetime
+                name="date"
+                closeOnSelect
+                initialValue={currentDate}
+                dateFormat="DD.MM.YYYY"
+                timeFormat={false}
+                isValidDate={checksFutureDate}
+                onChange={e => setFieldValue('date', new Date(e).toString())}
+                inputProps={{onKeyDown: e => e.preventDefault()}}
+                renderInput={(p, openCalendar) => (
+                <DateInput  props={p} onOpen={openCalendar}  />)}
+            />
+        </DateWrapper>
 
+        <Box marginBottom="0">
+            <FormInput name="comment" placeholder="Comment" />
+        </Box>
+
+        <ButtonAdd type="submit" disabled={isSubmitting} >{submitText}</ButtonAdd>
+        <ButtonCancel type="button" onClick={onCancelClick}>Cancel</ButtonCancel>
+    </>
+}
+ 
 export default FormTransaction;
