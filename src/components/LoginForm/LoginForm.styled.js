bcf813f4d5695b5d28e0f7778c9802aca34d0b94
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-18 13:55:51 +0200

    Update LoginForm.styled.js

--- src/components/LoginForm/LoginForm.styled.js
+++ src/components/LoginForm/LoginForm.styled.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
-import { Form, Field, ErrorMessage } from 'formik';
 
 export const FormWrap = styled.div`
   width: 340px;
-  padding: 32px 20px 36px;
+  padding: 30px 20px;
   background-color: rgba(0, 0, 0, 0.7);
   border: 10px solid rgba(255, 255, 255, 0.2);
   background-clip: padding-box;
@@ -13,54 +12,66 @@ export const FormWrap = styled.div`
   transform: ${p => (p.isScale ? 'scaleY(1)' : 'scaleY(0)')};
 
   @media screen and (min-width: 768px) {
-    width: 530px;
+    width: 500px;
     margin-right: 50px;
-    padding: 40px 58px 40px 65px;
+    padding: 36px 60px;
   }
 `;
 
 export const LogoWrap = styled.div`
   display: flex;
   justify-content: center;
+  margin-bottom: 60px;
 `;
 
-export const StyledForm = styled(Form)`
-  width: 100%;
-  margin-top: 60px;
-`;
 
-export const Label = styled.label`
-  position: relative;
-  display: flex;
-  align-items: center;
-  /* width: 100%; */
-  border-bottom: 1px solid ${p => p.theme.colors.secondaryLightText};
-  
-  
-  padding-left: 10px;
-  /* & + & {
-    margin-top: 40px;
-  } */
-`;
 
-export const Input = styled(Field)`
-  /* width: 240px; */
-  width: 100%;
-
-  padding: 5px 20px;
-  border: ${p => p.theme.borders.none};
+export const EyesButton = styled.button`
+  border: none;
   outline: none;
-  background-color: inherit;
-  color: ${p => p.theme.colors.white};
-  
-  ::placeholder {
-    color: ${p => p.theme.colors.secondaryText};
-  }
-
-  /* @media screen and (min-width: 768px) {
-    width: 370px;
-  } */
+  background: transparent;
+  cursor: pointer;
 `;
+
+
+
+
+
+
+
+
+
+
+// export const StyledForm = styled(Form)`
+//   width: 100%;
+//   margin-top: 60px;
+// `;
+
+
+// export const Label = styled.label`
+//   position: relative;
+//   display: flex;
+//   align-items: center;
+//   border-bottom: 1px solid ${p => p.theme.colors.secondaryLightText};
+  
+  
+//   padding-left: 10px;
+// `;
+
+// export const Input = styled(Field)`
+//   width: 100%;
+
+//   padding: 5px 20px;
+//   border: ${p => p.theme.borders.none};
+//   outline: none;
+//   background-color: inherit;
+//   color: ${p => p.theme.colors.white};
+  
+//   ::placeholder {
+//     color: ${p => p.theme.colors.secondaryText};
+//   }
+
+// `;
 
 // export const ErrorMsg = styled(ErrorMessage)`
 //   position: absolute;
@@ -72,21 +83,16 @@ export const Input = styled(Field)`
 //   background-color: transparent;
 // `;
 
-export const ErrorText = styled(ErrorMessage)`
-  position: absolute;
-  top: -20px;
-  left: 2px;
-  font-size: 16px;
-  letter-spacing: 0.05em;
-  color: #29ffd0;
-`;
+// export const ErrorText = styled(ErrorMessage)`
+//   position: absolute;
+//   top: -20px;
+//   left: 2px;
+//   font-size: 16px;
+//   letter-spacing: 0.05em;
+//   color: #29ffd0;
+// `;
 
-export const EyesButton = styled.button`
-  border: none;
-  outline: none;
-  background: transparent;
-  cursor: pointer;
-`;
+
 
 
 
