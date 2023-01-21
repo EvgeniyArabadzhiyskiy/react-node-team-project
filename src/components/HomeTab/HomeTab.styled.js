d86585327a038579084c5474ec620d31b5b0a07a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-21 22:05:11 +0200

    Update HomeTab.styled.js

--- src/components/HomeTab/HomeTab.styled.js
+++ src/components/HomeTab/HomeTab.styled.js
@@ -1,47 +1,35 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const StyledTableHeader = styled.div`
-  background-color: ${p => p.theme.colors.primaryBg};
-  font-weight: ${p => p.theme.fontWeights.bold};
+export const CommonStyle = css`
   font-size: ${p => p.theme.fontSizes.s};
   line-height: 1.5;
 
-  padding: 15px 20px;
-  border-radius: 30px;
-
-  @media (min-width: 768px) {
-    display: flex;
-  }
+  display: grid;
+  grid-template-columns: 15% 15% 20% 25% 10% 15%;
+  align-items: center;
 
   p {
-    @media (min-width: 768px) {
-      margin: 0;
-      &:nth-child(1) {
-        width: 15%;
-      }
-      &:nth-child(2) {
-        width: 15%;
-        display: flex;
-        justify-content: center;
-      }
-      &:nth-child(3) {
-        width: 20%;
-      }
-      &:nth-child(4) {
-        width: 25%;
-      }
-      &:nth-child(5) {
-        width: 10%;
-        display: flex;
-        justify-content: flex-end;
-      }
-      &:nth-child(6) {
-        width: 15%;
-        display: flex;
-        justify-content: flex-end;
-      }
+    &:nth-of-type(2) {
+      text-align: center;
+    }
+
+    &:nth-of-type(5) {
+      text-align: end;
+    }
+
+    &:nth-of-type(6) {
+      text-align: end;
     }
   }
+`;
+
+export const StyledTableHeader = styled.div`
+  ${CommonStyle}
+
+  font-weight: ${p => p.theme.fontWeights.bold};
+  background-color: ${p => p.theme.colors.primaryBg};
+  padding: 15px 20px;
+  border-radius: 30px;
 `;
 
 export const StyledTable = styled.div`
@@ -62,78 +50,57 @@ export const StyledTableBody = styled.ul`
   line-height: 1.5;
 
   overflow-y: scroll;
-  height: 260px;
+  max-height: 260px;
+`;
 
-  li {
-    align-items: center;
-    height: 52px;
-    &:not(:last-child) {
-      border-bottom: 1px solid #dcdcdf;
-    }
-    @media (min-width: 768px) {
-      display: flex;
+export const StyledItem = styled.li`
+  ${CommonStyle};
+
+  height: 52px;
+
+  &:not(:last-child) {
+    border-bottom: 1px solid #dcdcdf;
+  }
+
+  p {
+    &:nth-of-type(3) {
+      padding-right: 32px;
     }
   }
-  li > p {
-    margin: 0;
-    line-height: 1.12;
-    @media (min-width: 768px) {
-      &:nth-child(1) {
-        width: 15%;
-      }
-      &:nth-child(2) {
-        width: 15%;
-        display: flex;
-        justify-content: center;
-      }
-      &:nth-child(3) {
-        width: 20%;
-      }
-      span {
-        display: block;
-        max-width: 106px;
-      }
-      &:nth-child(4) {
-        width: 25%;
-      }
-      &:nth-child(5) {
-        width: 10%;
-        font-weight: ${p => p.theme.fontWeights.bold};
-        display: flex;
-        justify-content: flex-end;
-      }
-      &:nth-child(6) {
-        width: 15%;
-        display: flex;
-        justify-content: flex-end;
-      }
-    }
-  }
+`;
+
+export const CategoryName = styled.p`
+  color: ${p => p.theme.colors.primaryText};
 `;
 
 export const StyledList = styled.ul`
   margin: 0;
   border-radius: 10px;
   background-color: #ffffff;
+
+  line-height: 1.5;
+
+  border-left: 5px solid
+    ${p => (p.borders === 'income' ? '#24CCA7' : '#FF6596')};
+
   li {
-    font-weight: ${p => p.theme.fontWeights.bold};
-    font-size: ${p => p.theme.fontSizes.s};
-    line-height: 1.5;
+    display: flex;
+    justify-content: space-between;
+
     padding: 12px 20px;
+    font-weight: ${p => p.theme.fontWeights.bold};
+
     &:not(:last-child) {
       border-bottom: 1px solid #dcdcdf;
     }
-    display: flex;
-    justify-content: space-between;
   }
+
   li > span {
     font-weight: ${p => p.theme.fontWeights.normal};
-    font-size: ${p => p.theme.fontSizes.s};
-    line-height: 1.5;
   }
 `;
 
-export const StyledWrap = styled.div`
+export const StyledWrap = styled.ul`
   max-width: 280px;
   margin: auto;
   border-left: 5px;
@@ -145,6 +112,102 @@ export const StyledWrap = styled.div`
   }
 `;
 
-export const CategoryName = styled.p`
-  color: ${p => p.theme.colors.primaryText};
-`;
+//   export const StyledTableHeader = styled.div`
+//   background-color: ${p => p.theme.colors.primaryBg};
+//   font-weight: ${p => p.theme.fontWeights.bold};
+//   font-size: ${p => p.theme.fontSizes.s};
+//   line-height: 1.5;
+
+//   padding: 15px 20px;
+//   border-radius: 30px;
+
+//   @media (min-width: 768px) {
+//     display: flex;
+//   }
+
+//   p {
+//     @media (min-width: 768px) {
+//       margin: 0;
+//       &:nth-child(1) {
+//         width: 15%;
+//       }
+//       &:nth-child(2) {
+//         width: 15%;
+//         display: flex;
+//         justify-content: center;
+//       }
+//       &:nth-child(3) {
+//         width: 20%;
+//       }
+//       &:nth-child(4) {
+//         width: 25%;
+//       }
+//       &:nth-child(5) {
+//         width: 10%;
+//         display: flex;
+//         justify-content: flex-end;
+//       }
+//       &:nth-child(6) {
+//         width: 15%;
+//         display: flex;
+//         justify-content: flex-end;
+//       }
+//     }
+//   }
+// `;
+
+// export const StyledTableBody = styled.ul`
+//   margin: 0;
+//   padding: 0 15px 0 20px;
+//   font-size: ${p => p.theme.fontSizes.s};
+//   line-height: 1.5;
+
+//   overflow-y: scroll;
+//   height: 260px;
+
+//   li {
+//     align-items: center;
+//     height: 52px;
+//     &:not(:last-child) {
+//       border-bottom: 1px solid #dcdcdf;
+//     }
+//     @media (min-width: 768px) {
+//       display: flex;
+//     }
+//   }
+//   li > p {
+//     margin: 0;
+//     line-height: 1.12;
+//     @media (min-width: 768px) {
+//       &:nth-child(1) {
+//         width: 15%;
+//       }
+//       &:nth-child(2) {
+//         width: 15%;
+//         display: flex;
+//         justify-content: center;
+//       }
+//       &:nth-child(3) {
+//         width: 20%;
+//       }
+//       span {
+//         display: block;
+//         max-width: 106px;
+//       }
+//       &:nth-child(4) {
+//         width: 25%;
+//       }
+//       &:nth-child(5) {
+//         width: 10%;
+//         font-weight: ${p => p.theme.fontWeights.bold};
+//         display: flex;
+//         justify-content: flex-end;
+//       }
+//       &:nth-child(6) {
+//         width: 15%;
+//         display: flex;
+//         justify-content: flex-end;
+//       }
+//     }
+//   }
+// `;
