aede678c7b1714e0735642ba3ddb40de83c37a28
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-21 22:05:42 +0200

    Create EnterButton.styled.jsx

---
+++ src/components/Buttons/EnterButton/EnterButton.styled.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import { baseBtnStyle, spin } from '../Buttons.styled';
+
+export const StyledEnterBtn = styled.button`
+  ${baseBtnStyle}
+
+  position: relative;
+  color: ${p => p.theme.colors.primaryBg};
+  border: 7px solid rgba(55, 53, 53, 0.4);
+  margin-top: ${p => p.theme.space[5]}px;
+  background-color: ${p => p.theme.colors.primaryBgBtn};
+
+  /* &:hover {
+  border: 7px solid ${p => p.theme.colors.primaryBgBtn};
+
+} */
+
+  /* transition: border 300ms linear; */
+
+  :disabled {
+    opacity: 0.7;
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+  }
+
+  &:hover,
+  &:focus {
+    /* border: 4px solid rgb(255, 3, 3); */
+    animation: 2000ms ${spin} infinite;
+  }
+`;
