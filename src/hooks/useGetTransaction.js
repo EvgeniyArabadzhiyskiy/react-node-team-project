d9c370bb55a9dbd4e7f717d21dfb2376bf8785dd
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-21 22:04:40 +0200

    Create useGetTransaction.js

---
+++ src/hooks/useGetTransaction.js
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTransactions, setUnmount } from 'redux/transactions/transactionsSlice';
+import { useGetAllTransactionsQuery } from 'redux/walletsApiServise/wallet-api';
+
+export const useGetTransaction = () => {
+  const dispatch = useDispatch();
+  const { transactions, pageNum } = useSelector(state => state.transactions);
+
+  const { data = {} } = useGetAllTransactionsQuery({ pageNum, limit: 10 });
+
+  useEffect(() => {
+    return () => {
+      dispatch(setUnmount(true));
+    };
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!data.transactions) return;
+    dispatch(getTransactions(data));
+  }, [data, dispatch]);
+
+  return transactions;
+};
