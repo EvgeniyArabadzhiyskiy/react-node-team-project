75cc682227f95ed17bd3791bd28bcfe58e541413
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-21 22:04:57 +0200

    Create IconButton.styled.jsx

---
+++ src/components/IconButton/IconButton.styled.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin: 0;
+  padding: 5px;
+  width: 44px;
+  height: 44px;
+
+  border: none;
+  outline: none;
+  border-radius: 50%;
+  background-color: #24cca7;
+
+  cursor: pointer;
+`;
