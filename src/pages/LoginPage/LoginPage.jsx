60ba33e381c0bb7031fa15e675c5bcd56313193b
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-21 22:04:38 +0200

    Update LoginPage.jsx

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,6 @@
 import { useLocation } from 'react-router-dom';
 
 import LoginForm from 'components/LoginForm';
-// import SharedLayout from 'components/SharedLayout';
 import RegistrationForm from 'components/RegistrationForm';
 import { Section, Container, Wrapper } from './LoginPage.styled';
 
