8c27cb78c63160340993a13fef5f4914cdc27492
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-24 17:21:32 +0200

    Update useBalanceList.js

--- src/hooks/useBalanceList.js
+++ src/hooks/useBalanceList.js
@@ -1,12 +1,36 @@
 import { getBalances } from 'helpers/formAddTransaction/getBalance';
 import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import { useGetBalanceQuery } from 'redux/walletsApiServise/wallet-api';
 
+
+const arrayAAA = [1,2,3,4,5,6,7,8,9,10]
+
+
+const arrayBBB = [9, 10]
+
+
+const filtredArr = arrayBBB.reduce((acc, ggg) => {
+  const kkk = acc.filter(item => item !== ggg)
+
+  return kkk
+},[...arrayAAA])
+
+// console.log("filtredArr  filtredArr", filtredArr);
+
+
+
+
+
+
 export const useBalanceList = transactions => {
-  console.log("useBalanceList  transactions", transactions);
+
+  const {  removedSum } = useSelector(state => state.transactions);
+  // console.log("useBalanceList  removedSum", removedSum);
+
   const { data: balance = {} } = useGetBalanceQuery();
   const totalBalance = balance.userBalance 
-  // - 1000;
+  - removedSum;
 
   const balances = useMemo(
     () => getBalances(transactions, totalBalance),
