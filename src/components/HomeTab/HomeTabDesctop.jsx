1cb484dccde568123c693b2f2673fbc70f900eea
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-24 10:02:20 +0200

    Update HomeTabDesctop.jsx

--- src/components/HomeTab/HomeTabDesctop.jsx
+++ src/components/HomeTab/HomeTabDesctop.jsx
@@ -1,40 +1,55 @@
 import { HomeTabItem } from "./HomeTabItem";
 import { CategoryName, StyledTable, StyledTableBody, StyledTableHeader } from "./HomeTab.styled";
 
-const HomeTabDesctop = ({ transactions, balances, lastElement }) => {
-    return <StyledTable>
-    <StyledTableHeader>
-      <CategoryName>Date</CategoryName>
-      <CategoryName>Type</CategoryName>
-      <CategoryName>Category</CategoryName>
-      <CategoryName>Comment</CategoryName>
-      <CategoryName>Sum</CategoryName>
-      <CategoryName>Balance</CategoryName>
-    </StyledTableHeader>
+const HomeTabDesctop = ({ transactions, balances, lastElement, getTransId
+  //  handleClick, clear 
+  }) => {
 
-    { transactions.length > 0 && <StyledTableBody>
-      {transactions.map((transaction, idx ) => {
-        const itemBalance = balances[idx];
+  return (
+    <StyledTable>
+      <StyledTableHeader>
+        <CategoryName>Date</CategoryName>
+        <CategoryName>Type</CategoryName>
+        <CategoryName>Category</CategoryName>
+        <CategoryName>Comment</CategoryName>
+        <CategoryName>Sum</CategoryName>
+        <CategoryName>Balance</CategoryName>
+      </StyledTableHeader>
 
-        if (transactions.length === idx + 1) {
+      { transactions.length > 0 && <StyledTableBody>
+        {transactions.map((transaction, idx ) => {
+          const itemBalance = balances[idx];
+
+          if (transactions.length === idx + 1) {
+            return (
+              <HomeTabItem
+                key={transaction._id}
+                ref={lastElement}
+
+                getTransId={getTransId}
+                // clear={clear}
+                // handleClick={handleClick}
+
+                transaction={{...transaction, itemBalance }}
+              />
+            )
+          }
+
           return (
             <HomeTabItem
               key={transaction._id}
-              ref={lastElement}
-              transaction={{...transaction, ...itemBalance }}
-            />
-          )
-        }
 
-        return (
-          <HomeTabItem
-            key={transaction._id}
-            transaction={{...transaction, ...itemBalance }}
-          />
+              getTransId={getTransId}
+              // clear={clear}
+              // handleClick={handleClick}
+
+              transaction={{...transaction, itemBalance }}
+            />
+          )}
         )}
-      )}
-    </StyledTableBody>}
-  </StyledTable>
+      </StyledTableBody>}
+    </StyledTable>
+  )
 }
  
 export default HomeTabDesctop;
