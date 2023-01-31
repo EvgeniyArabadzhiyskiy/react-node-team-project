fb74888e42f1173e21a049d52a19379f89ebd3a2
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-31 19:22:33 +0200

    Create ContextMenu.jsx

---
+++ src/components/ContextMenu/ContextMenu.jsx
@@ -0,0 +1,16 @@
+import { RxCross2 } from 'react-icons/rx';
+import { CloseBtn, DeleteBtn, Menu } from './ContextMenu.styled';
+
+const ContextMenu = ({ isOpenMenu, isDelete, onContextMenu, onEdit, onDelete, onClearId }) => {
+    
+  return (
+    <Menu isOpenMenu={isOpenMenu} isDelete={isDelete}>
+      <CloseBtn onClick={onContextMenu}><RxCross2 /></CloseBtn>
+      <DeleteBtn onClick={onEdit}>EDIT</DeleteBtn>
+      {isDelete && <DeleteBtn onClick={onDelete}>DELETE</DeleteBtn>}
+      {!isDelete && <DeleteBtn onClick={onClearId}>CANCEL</DeleteBtn>}
+    </Menu>
+  );
+};
+
+export default ContextMenu;
