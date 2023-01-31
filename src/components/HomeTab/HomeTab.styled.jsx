020b7cc386a3be36f2e387b614f0df8f1bf4b83e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-31 19:22:14 +0200

    Update HomeTab.styled.jsx

--- src/components/HomeTab/HomeTab.styled.jsx
+++ src/components/HomeTab/HomeTab.styled.jsx
@@ -77,42 +77,47 @@ export const StyledItem = styled.li`
   }
 `;
 
-export const ContextMenu = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-color: #3d3a3a92;
+// export const Menu = styled.div`
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   width: 100%;
+//   height: 100%;
+//   background-color: #3d3a3a92;
 
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
+//   display: flex;
+//   justify-content: space-evenly;
+//   align-items: center;
 
-  transform: translateX(100%);
-  transition: transform 300ms linear;
-  transform: ${p => p.isOpenMenu && 'translateX(0%)'};
+//   transform: translateX(100%);
+//   transition: transform 300ms linear;
+//   transform: ${p => p.isOpenMenu && 'translateX(0%)'};
 
-  &::before {
-    content: '';
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 5px;
-    background-color: #5009d2fc;
+//   &::before {
+//     content: '';
+//     position: absolute;
+//     bottom: 0;
+//     left: 0;
+//     width: 100%;
+//     height: 5px;
+//     background-color: #5009d2fc;
 
-    transform: translateX(0%);
+//     transform: translateX(0%);
 
-    transition: ${p => !p.isDelete && 'transform 6000ms linear'};
+//     transition: ${p => !p.isDelete && 'transform 3000ms linear'};
 
-    transform: ${p => p.isDelete && 'translateX(100%)'};
-  }
-`;
+//     transform: ${p => p.isDelete && 'translateX(100%)'};
+//   }
+// `;
 
 export const CategoryName = styled.p`
   color: ${p => p.theme.colors.primaryText};
 `;
 
 export const StyledList = styled.ul`
+  position: relative;
+  overflow: hidden;
+
   margin: 0;
   border-radius: 10px;
   background-color: #ffffff;
@@ -151,16 +156,17 @@ export const StyledWrap = styled.ul`
   }
 `;
 
-export const DeleteBtn = styled.button`
-  width: 100px;
-  height: 40px;
-  border: 7px solid rgba(55, 53, 53, 0.4);
-  color: ${p => p.theme.colors.primaryBg};
-  background-color: ${p => p.theme.colors.primaryBgBtn};
+// export const DeleteBtn = styled.button`
+//   width: 100px;
+//   height: 40px;
+//   font-size: 16px;
+//   border: 7px solid rgba(55, 53, 53, 0.4);
+//   color: ${p => p.theme.colors.primaryBg};
+//   background-color: ${p => p.theme.colors.primaryBgBtn};
 
-  cursor: pointer;
-  text-transform: uppercase;
-`;
+//   cursor: pointer;
+//   text-transform: uppercase;
+// `;
 
 export const SettingBtn = styled.button`
   background: transparent;
@@ -175,16 +181,16 @@ export const SettingBtn = styled.button`
   }
 `;
 
-export const CloseBtn = styled(SettingBtn)`
-  position: absolute;
-  top: 6px;
-  left: 6px;
-  border-radius: 50%;
-  background-color: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0;
-  width: 20px;
-  height: 20px;
-`;
+// export const CloseBtn = styled(SettingBtn)`
+//   position: absolute;
+//   top: 6px;
+//   left: 6px;
+//   border-radius: 50%;
+//   background-color: white;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   padding: 0;
+//   width: 20px;
+//   height: 20px;
+// `;
