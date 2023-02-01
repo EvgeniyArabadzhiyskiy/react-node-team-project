c282c1dce4264067500377577ed835bc2f920e2e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-01 23:37:29 +0200

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -9,16 +9,15 @@ const Logo = () => {
   const isDesctop = useMedia('(min-width: 768px)');
 
   const isLoginPage =
-  location.pathname === '/login' || 
-  location.pathname === '/register';
+    location.pathname === '/login' || location.pathname === '/register';
 
   return (
     <LogoBox>
       <LogoLink to={isLoginPage ? '/login' : '/home'}>
         {isDesctop ? (
-          <LogoWallet src={logoBig} alt="logo" />
+          <LogoWallet src={logoBig} width="40" height="40" alt="logo" />
         ) : (
-          <LogoWallet src={logo} alt="logo" />
+          <LogoWallet src={logo} width="30" height="30" alt="logo" />
         )}
         <LogoText isLoginPage={isLoginPage}>Wallet</LogoText>
       </LogoLink>
