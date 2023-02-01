489d53618e396823a3a65c1b15e901e5e800a508
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-01 23:38:13 +0200

    Create EyesButton.jsx

---
+++ src/components/Buttons/EyesButton/EyesButton.jsx
@@ -0,0 +1,12 @@
+import { HiEyeOff, HiEye } from 'react-icons/hi';
+import { StyledButton } from 'components/LoginForm/LoginForm.styled';
+
+const EyesButton = ({ isHidePass, setIsHidePass }) => {
+  return (
+    <StyledButton type="button" onClick={() => setIsHidePass(s => !s)} aria-label="show-pass">
+      {isHidePass ? <HiEye color="white" /> : <HiEyeOff color="white" />}
+    </StyledButton>
+  );
+};
+
+export default EyesButton;
