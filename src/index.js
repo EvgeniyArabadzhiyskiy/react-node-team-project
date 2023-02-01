31b4531f741f876db056d7d9669c46426d284d85
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-01 23:37:47 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -7,20 +6,17 @@ import { App } from 'components/App';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 // import { FontStyles } from 'components/FontStyles';
-import './index.css'
-
-// axios.defaults.baseURL = 'https://wallet-project.onrender.com/api';
-axios.defaults.baseURL = 'https://wallet-backend-xmk0.onrender.com/api';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/react-node-team-project">
-          {/* <FontStyles /> */}
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/react-node-team-project">
+        {/* <FontStyles /> */}
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
   // </React.StrictMode>
 );
