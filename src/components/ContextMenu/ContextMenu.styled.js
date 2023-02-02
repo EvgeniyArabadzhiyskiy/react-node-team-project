06558fd740081a7b2e0f4328dc89aa40128930fd
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-02 19:10:22 +0200

    Create ContextMenu.styled.js

---
+++ src/components/ContextMenu/ContextMenu.styled.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+export const Menu = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #3d3a3a92;
+
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+
+  transform: translateX(100%);
+  transition: transform 300ms linear;
+  transform: ${p => p.isOpenMenu && 'translateX(0%)'};
+
+  &::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 5px;
+    background-color: #5009d2fc;
+
+    transform: translateX(0%);
+
+    transition: ${p => !p.isDelete && 'transform 3000ms linear'};
+
+    transform: ${p => p.isDelete && 'translateX(100%)'};
+  }
+`;
+
+export const DeleteBtn = styled.button`
+  width: 100px;
+  height: 40px;
+  font-size: 16px;
+  border: 7px solid rgba(55, 53, 53, 0.4);
+  color: ${p => p.theme.colors.primaryBg};
+  background-color: ${p => p.theme.colors.primaryBgBtn};
+
+  cursor: pointer;
+  text-transform: uppercase;
+`;
+
+export const SettingBtn = styled.button`
+  background: transparent;
+
+  svg {
+    display: block;
+    transition: fill 250ms linear;
+  }
+
+  svg:hover {
+    fill: ${p => p.theme.colors.primaryBgBtn};
+  }
+`;
+
+export const CloseBtn = styled(SettingBtn)`
+  position: absolute;
+  top: 6px;
+  left: 6px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  width: 20px;
+  height: 20px;
+`;
