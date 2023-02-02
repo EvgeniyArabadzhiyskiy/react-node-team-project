76b7f81a300768032f0a498451ed5a3de46c1bb1
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-02 19:09:40 +0200

    Create DetailsMobileItem.styled.js

---
+++ src/components/HomeTab/DetailsMobileItem/DetailsMobileItem.styled.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const StyledList = styled.ul`
+  position: relative;
+  overflow: hidden;
+
+  margin: 0;
+  border-radius: 10px;
+  background-color: #ffffff;
+
+  line-height: 1.5;
+
+  border-left: 5px solid
+    ${p => (p.borders === 'income' ? '#24CCA7' : '#FF6596')};
+
+  li {
+    display: flex;
+    justify-content: space-between;
+
+    padding: 12px 20px;
+    font-weight: ${p => p.theme.fontWeights.bold};
+
+    &:not(:last-child) {
+      border-bottom: 1px solid #dcdcdf;
+    }
+  }
+
+  li > span {
+    font-weight: ${p => p.theme.fontWeights.normal};
+  }
+`;
+
+export const SumText = styled.span`
+  color: ${p => (p.textColor === '+' ? '#24CCA7' : '#FF6596')};
+`;
