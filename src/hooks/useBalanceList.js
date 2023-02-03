af3a1d6eb2677eadc2fd95f22f9aaec551194ce4
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-03 22:27:35 +0200

    Update useBalanceList.js

--- src/hooks/useBalanceList.js
+++ src/hooks/useBalanceList.js
@@ -3,8 +3,7 @@ import { getBalances } from 'helpers/formAddTransaction/getBalance';
 import { useGetBalanceQuery } from 'redux/walletsApiServise/wallet-api';
 
 export const useBalanceList = transactions => {
-  const { data: balance = {} } = useGetBalanceQuery();
-  const totalBalance = balance.userBalance;
+  const { data: totalBalance = 0 } = useGetBalanceQuery();
 
   const balances = useMemo(
     () => getBalances(transactions, totalBalance),
