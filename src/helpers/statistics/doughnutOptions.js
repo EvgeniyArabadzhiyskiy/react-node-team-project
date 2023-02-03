c0639aa135842f9ab47a6678332f91809b1b6204
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-03 22:27:31 +0200

    Update doughnutOptions.js

--- src/helpers/statistics/doughnutOptions.js
+++ src/helpers/statistics/doughnutOptions.js
@@ -1,18 +1,6 @@
-export const drawInnerText = (chart, totalBalance) => {
-  const ctx = chart.ctx;
-  ctx.restore();
-  ctx.font = '700 18px Circe-Regular';
-  ctx.textBaseline = 'middle';
-  const text = `₴${totalBalance}`;
-  const textX = Math.round((chart.width - ctx.measureText(text).width) / 2);
-  const textY = chart.height / 2 + chart.legend.height / 2;
-  ctx.fillText(text, textX, textY);
-  ctx.fillStyle = '#000000';
-  ctx.save();
-};
-
 export const options = {
   cutout: '70%',
+  aspectRatio: 1,
   plugins: {
     legend: {
       display: false,
