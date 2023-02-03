7e26ffa838ea1e211313c1e32413778b92c63586
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-03 22:27:33 +0200

    Update getCategoryColor.js

--- src/helpers/statistics/getCategoryColor.js
+++ src/helpers/statistics/getCategoryColor.js
@@ -1,12 +1,12 @@
 const categoryList = {
-  Food: '#FED057',
-  Main: '#FFD8D0',
-  House: '#C5BAFF',
-  Auto: '#6E78E8',
-  Children: '#81E1FF',
-  Development: '#FD9498',
-  Education: '#24CCA7',
-  'Other expenses': '#00AD84',
+  Food: '#f6a626',
+  Main: '#f4ee2b',
+  House: '#652ed3',
+  Auto: '#4bc0c0',
+  Children: '#36a3eb',
+  Development: '#ff6385',
+  Education: '#342ee9',
+  'Other expenses': '#00ad31',
 };
 
 export const getCategoryColor = value => categoryList[value];
