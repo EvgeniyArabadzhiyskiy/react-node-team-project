fee8e8b7b90bb61dfb884aac3582373cdb6f7de0
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-04 15:58:18 +0200

    Update DiagramTab.jsx

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -1,37 +1,30 @@
 import { Box } from 'components/Box';
 import { useMedia } from 'react-use';
+import { breakpoints } from 'styles/breakpoints';
 
 import Chart from '../Chart';
 import Table from '../Table';
-
-import {
-  StyledChartWraper,
-  StyledTitle,
-} from './DiagramTab.styled';
+import { StyledChartWraper,  StyledTitle } from './DiagramTab.styled';
 
 const DiagramTab = () => {
-  const isMobie = useMedia('(max-width: 767px)');
+  const isMobie = useMedia(breakpoints.small);
+  const isDesctop = useMedia(breakpoints.medium);
 
   return (
     <>
-      {isMobie ? (
+      { isMobie && (
         <div>
           <StyledTitle>Statistics</StyledTitle>
-
-          <StyledChartWraper>
-            <Chart />
-          </StyledChartWraper>
-
+          <StyledChartWraper><Chart /></StyledChartWraper>
           <Table />
         </div>
-      ) : (
+      )} 
+
+      { isDesctop && (
         <div>
           <StyledTitle>Statistics</StyledTitle>
-          <Box display="flex">
-            <StyledChartWraper>
-              <Chart />
-            </StyledChartWraper>
-
+          <Box display="flex" justifyContent="space-between">
+            <StyledChartWraper><Chart /></StyledChartWraper>
             <Table />
           </Box>
         </div>
