225f44acc807ba90aa478e218ea62ff9398df786
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-02-04 15:58:51 +0200

    Update Table.jsx

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,4 +1,3 @@
-import { useMedia } from 'react-use';
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
@@ -13,135 +12,71 @@ import {
   StyledTableFooter,
   StyledFilters,
   StyledItem,
-  StyledInnerSpan,
+  ColorSpan,
+  StyledText,
 } from './Table.styled';
 import FilterDate from './FilterDate';
 
 
 const Table = () => {
   const dispatch = useDispatch();
-  const isMobile = useMedia('(max-width: 767px)');
   
   const [year, setYear] = useState(null);
   const [month, setMonth] = useState(null);
 
-  const { data = [] } = useGetStatisticQuery({ month, year },{
-    refetchOnMountOrArgChange: year || month,
-  });
+  const { data = [] } = useGetStatisticQuery({ month, year });
 
   useEffect(() => {
     dispatch(getStatisticDate({ month, year }));
   }, [dispatch, month, year]);
 
+  const expensesData = data
+    .filter(data => data.type !== 'income')  
+
+  const expensesTotal = expensesData
+    .reduce((total, data) => total + Number(data.totalSum), 0);
+
   const incomeTotal = data
     .filter(data => data.type === 'income')
     .reduce((total, data) => total + Number(data.totalSum), 0);
 
-  const expensesTotal = data
-    .filter(data => data.type !== 'income')
-    .reduce((total, data) => total + Number(data.totalSum), 0);
+  return (  
+    <StyledTable>
+      <StyledFilters>
+        <FilterDate setMonth={setMonth}  />
+        <FilterDate  setYear={setYear} value={"Year"}/>
+      </StyledFilters>
+      
+      <StyledTableHeader>
+        <p>Category</p>
+        <p>Sum</p>
+      </StyledTableHeader>
 
-  return (
-    <>
-      {isMobile ? (
-        <StyledTable>
-          <div>
-            <StyledFilters>
-              <FilterDate setMonth={setMonth}  />
-              <FilterDate  setYear={setYear} value={"Year"}/>
-            </StyledFilters>
-          </div>
-          <StyledTableHeader>
-            <p>Category</p>
-            <p>Sum</p>
-          </StyledTableHeader>
-          <StyledTableBody> 
-              {data.map(({ _id, type, totalSum }) => {
-                if (type === 'expense') {
-                  return (
-                    <li key={_id}>
-                      <StyledItem>
-                        <StyledInnerSpan
-                          category={getCategoryColor(_id)}
-                          style={{
-                            width: '24px',
-                            height: '24px',
-                            marginRight: '10px',
-                          }}
-                        ></StyledInnerSpan>
-                        <p>{_id}</p>
-                      </StyledItem>
-                      <p>
-                        <span>{totalSum}</span>
-                      </p>
-                    </li>
-                  );
-                }
-                return null;
-              })}            
-          </StyledTableBody>
-          <StyledTableFooter>
-            <li>
-              <p>Expenses:</p>
-              <p style={{ color: '#FF6596' }}>{expensesTotal}</p>
+      <StyledTableBody> 
+        { expensesData.map(({ _id: color, totalSum }) => {
+          return (
+            <li key={color}>
+              <StyledItem>
+                <ColorSpan category={getCategoryColor(color)}/>
+                {color}
+              </StyledItem>
+              <p>{totalSum}</p>
             </li>
-            <li>
-              <p>Income:</p>
-              <p style={{ color: '#24CCA7' }}>{incomeTotal}</p>
-            </li>
-          </StyledTableFooter>
-        </StyledTable>
-      ) : (
-        <StyledTable>
-          <div>
-            <StyledFilters>
-              <FilterDate setMonth={setMonth}  />
-              <FilterDate  setYear={setYear} value={"Year"}/> 
-            </StyledFilters>
-          </div>
-          <StyledTableHeader>
-            <p>Category</p>
-            <p>Sum</p>
-          </StyledTableHeader>
+          );
+        })}            
+      </StyledTableBody>
 
-          <StyledTableBody>
-              {data.map(({ _id, type, totalSum }) => {
-                if (type === 'expense') {
-                  return (
-                    <li key={_id}>
-                      <StyledItem>
-                        <StyledInnerSpan
-                          category={getCategoryColor(_id)}
-                          style={{
-                            width: '24px',
-                            height: '24px',
-                            marginRight: '10px',
-                          }}
-                        ></StyledInnerSpan>
-                        <p>{_id}</p>
-                      </StyledItem>
-                      <p>
-                        <span>{totalSum}</span>
-                      </p>
-                    </li>
-                  );
-                }
-                return null;
-              })}
-          </StyledTableBody>
-          <StyledTableFooter>
-            <li>
-              <p>Expenses:</p>
-              <p style={{ color: '#FF6596' }}>{expensesTotal}</p>
-            </li>
-            <li>
-              <p>Income:</p>
-              <p style={{ color: '#24CCA7' }}>{incomeTotal}</p>
-            </li>
-          </StyledTableFooter>
-        </StyledTable>
-      )}
-    </>
+      <StyledTableFooter>
+        <li>
+          <p>Expenses:</p>
+          <StyledText type="expense">{expensesTotal}</StyledText>
+        </li>
+        <li>
+          <p>Income:</p>
+          <StyledText type="income">{incomeTotal}</StyledText>
+        </li>
+      </StyledTableFooter>
+    </StyledTable>
   );
 };
 
